27fa6ee34f74cb8b8e5812f1da435d4491a415aa
kath <kathleen_tan@dlsu.edu.ph>
2024-02-12 22:59:41 +0800

    fixed Navbar, added description to Payment Record Table, and changed title to purpose in I/E table

--- app/(routes)/admin/finance/income-and-expense/_components/columns.tsx
+++ app/(routes)/admin/finance/income-and-expense/_components/columns.tsx
@@ -1,119 +1,119 @@
-'use client'
+"use client";
 
-import { ColumnDef } from '@tanstack/react-table'
-import { CellAction } from './cell-action'
-import { ArrowUpDown } from 'lucide-react'
-import { Button } from '@/components/ui/button'
+import { ColumnDef } from "@tanstack/react-table";
+import { CellAction } from "./cell-action";
+import { ArrowUpDown } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 export type TransactionColumn = {
-  id: string
-  dateSubmitted: string
-  dateIssued: string
-  type: string
-  title: string
-  amount: string
-  description: string
-}
+  id: string;
+  dateSubmitted: string;
+  dateIssued: string;
+  type: string;
+  purpose: string;
+  amount: string;
+  description: string;
+};
 
 export const columns: ColumnDef<TransactionColumn>[] = [
   {
-    accessorKey: 'dateSubmitted',
+    accessorKey: "dateSubmitted",
     header: ({ column }) => {
       return (
         <Button
-          variant='ghost'
-          className='hover:bg-[#ffe492]'
-          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+          variant="ghost"
+          className="hover:bg-[#ffe492]"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
         >
           Date Submitted
-          <ArrowUpDown className='w-4 h-4 ml-2' />
+          <ArrowUpDown className="w-4 h-4 ml-2" />
         </Button>
-      )
+      );
     },
-    cell: ({ row }) => row.getValue('address')
+    cell: ({ row }) => row.getValue("dateSubmitted"),
   },
   {
-    accessorKey: 'dateIssued',
+    accessorKey: "dateIssued",
     header: ({ column }) => {
       return (
         <Button
-          variant='ghost'
-          className='hover:bg-[#ffe492]'
-          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+          variant="ghost"
+          className="hover:bg-[#ffe492]"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
         >
           Date Issued
-          <ArrowUpDown className='w-4 h-4 ml-2' />
+          <ArrowUpDown className="w-4 h-4 ml-2" />
         </Button>
-      )
+      );
     },
-    cell: ({ row }) => row.getValue('lotNumber')
+    cell: ({ row }) => row.getValue("dateIssued"),
   },
   {
-    accessorKey: 'type',
+    accessorKey: "type",
     header: ({ column }) => {
       return (
         <Button
-          variant='ghost'
-          className='hover:bg-[#ffe492]'
-          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+          variant="ghost"
+          className="hover:bg-[#ffe492]"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
         >
           Type
-          <ArrowUpDown className='w-4 h-4 ml-2' />
+          <ArrowUpDown className="w-4 h-4 ml-2" />
         </Button>
-      )
+      );
     },
-    cell: ({ row }) => row.getValue('lotSize')
+    cell: ({ row }) => row.getValue("type"),
   },
   {
-    accessorKey: 'title',
+    accessorKey: "purpose",
     header: ({ column }) => {
       return (
         <Button
-          variant='ghost'
-          className='hover:bg-[#ffe492]'
-          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+          variant="ghost"
+          className="hover:bg-[#ffe492]"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
         >
-          Title
-          <ArrowUpDown className='w-4 h-4 ml-2' />
+          Purpose
+          <ArrowUpDown className="w-4 h-4 ml-2" />
         </Button>
-      )
+      );
     },
-    cell: ({ row }) => row.getValue('occupantName')
+    cell: ({ row }) => row.getValue("purpose"),
   },
   {
-    accessorKey: 'amount',
+    accessorKey: "amount",
     header: ({ column }) => {
       return (
         <Button
-          variant='ghost'
-          className='hover:bg-[#ffe492]'
-          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+          variant="ghost"
+          className="hover:bg-[#ffe492]"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
         >
           Amount
-          <ArrowUpDown className='w-4 h-4 ml-2' />
+          <ArrowUpDown className="w-4 h-4 ml-2" />
         </Button>
-      )
+      );
     },
-    cell: ({ row }) => row.getValue('purchaseDate')
+    cell: ({ row }) => row.getValue("amount"),
   },
   {
-    accessorKey: 'description',
+    accessorKey: "description",
     header: ({ column }) => {
       return (
         <Button
-          variant='ghost'
-          className='hover:bg-[#ffe492]'
-          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+          variant="ghost"
+          className="hover:bg-[#ffe492]"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
         >
           Description
-          <ArrowUpDown className='w-4 h-4 ml-2' />
+          <ArrowUpDown className="w-4 h-4 ml-2" />
         </Button>
-      )
+      );
     },
-    cell: ({ row }) => row.getValue('purchaseDate')
+    cell: ({ row }) => row.getValue("description"),
   },
   {
-    id: 'actions',
-    cell: ({ row }) => <CellAction data={row.original} />
-  }
-]
+    id: "actions",
+    cell: ({ row }) => <CellAction data={row.original} />,
+  },
+];
