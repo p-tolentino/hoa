eddb9ec2f6930a7de7def08d4a278865c21959cb
kath <kathleen_tan@dlsu.edu.ph>
2024-02-27 01:36:03 +0800

    updated create poll and survey modal

--- app/(routes)/admin/community/_components/pollsAndSurveys/_create/polls.tsx
+++ app/(routes)/admin/community/_components/pollsAndSurveys/_create/polls.tsx
@@ -12,17 +12,43 @@ import {
 import {
   Input,
   Stack,
-  Text,
   Box,
   HStack,
   Divider,
   Checkbox,
   CheckboxGroup,
   Button,
+  FormControl,
+  FormLabel,
+  Textarea,
+  FormHelperText,
 } from "@chakra-ui/react";
 import { AddIcon } from "@chakra-ui/icons";
+import { useState } from "react";
 
 function createPoll() {
+  let [pollDescription, setPollDescription] = useState("");
+  let [options, setOptions] = useState([""]);
+
+  let handlePollDescriptionChange = (
+    e: React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    let inputPollDescription = e.target.value;
+    setPollDescription(inputPollDescription);
+  };
+
+  let [isNewCategorySelected, setIsNewCategorySelected] = useState(false);
+  let [newCategory, setNewCategory] = useState("");
+
+  let handleCheckboxChange = () => {
+    setIsNewCategorySelected(!isNewCategorySelected);
+    setNewCategory(""); // Clear the input field when toggling the checkbox
+  };
+
+  let addOption = () => {
+    setOptions([...options, ""]);
+  };
+
   return (
     <Dialog /*open={open} onOpenChange={setOpen}*/>
       <DialogTrigger asChild>
@@ -31,80 +57,133 @@ function createPoll() {
           Create Poll
         </Button>
       </DialogTrigger>
-      <DialogContent className="">
-        <DialogHeader>
-          <DialogTitle>Create a Poll</DialogTitle>
-          <DialogDescription>
-            Fill up the following poll details.
-          </DialogDescription>
-        </DialogHeader>
+      <DialogContent className="lg:min-w-[800px]">
+        <form action="">
+          <DialogHeader>
+            <DialogTitle>Create a Poll</DialogTitle>
+            <DialogDescription>
+              Fill up the following fields to create a poll.
+            </DialogDescription>
+          </DialogHeader>
 
-        {/* Form Content */}
-        <Stack spacing="15px">
-          <Text fontSize="sm" fontWeight="semibold">
-            Poll Title:
-          </Text>
-          <Input size="sm" type="string" placeholder="Enter a Poll Title" />
-          <Text fontSize="sm" fontWeight="semibold">
-            Description:
-          </Text>
-          <Input size="sm" type="string" placeholder="Enter a Description" />
-
-          {/* Select Category */}
-          <Text fontSize="sm" fontWeight="semibold">
-            Category:
-          </Text>
-          <CheckboxGroup size="sm" colorScheme="yellow">
-            <Stack spacing={5} direction="row" fontFamily="font.body">
-              <Checkbox>Meeting</Checkbox>
-              <Checkbox>Election</Checkbox>
-              <Checkbox>Inquiry</Checkbox>
-              <Checkbox>Event</Checkbox>
-            </Stack>
-          </CheckboxGroup>
-          <Text fontSize="sm" fontWeight="semibold">
-            Add Category:
-          </Text>
-          <Input size="sm" type="string" placeholder="Enter a new category" />
-          <Divider />
-
-          {/* Poll Question */}
-          <Box p="10px">
-            <Stack spacing="15px">
-              <Text fontSize="sm" fontWeight="semibold">
-                Poll Question:
-              </Text>
+          {/* Form Content */}
+          <Stack spacing="15px" my="2rem">
+            <FormControl isRequired>
+              <FormLabel fontSize="sm" fontWeight="semibold">
+                Poll Title:
+              </FormLabel>
               <Input
-                size="sm"
+                size="md"
+                fontWeight="semibold"
                 type="string"
-                placeholder="Enter a Poll Question"
+                placeholder="Enter a Poll Title"
               />
-              <Text fontSize="sm" fontWeight="semibold">
-                Options:
-              </Text>
-              <HStack>
-                <Input size="sm" type="string" placeholder="Option 1" />
-                <Box alignSelf="center" ml="2%">
-                  <Button size="xs" w="20px">
-                    <AddIcon />
-                  </Button>
-                </Box>
-              </HStack>
-            </Stack>
-          </Box>
-        </Stack>
+            </FormControl>
+            <FormControl isRequired>
+              <FormLabel fontSize="sm" fontWeight="semibold">
+                Description:
+              </FormLabel>
+              <Textarea
+                placeholder="Write something..."
+                id="pollDescription"
+                fontSize="xs"
+                maxH="300px"
+                value={pollDescription}
+                onChange={handlePollDescriptionChange}
+              />
+            </FormControl>
 
-        <DialogFooter>
-          <Button
-            w="full"
-            size="sm"
-            colorScheme="yellow"
-            type="submit"
-            // onClick={() => onSubmit()}
-          >
-            Publish
-          </Button>
-        </DialogFooter>
+            {/* Select Category */}
+            <FormControl isRequired>
+              <FormLabel fontSize="sm" fontWeight="semibold">
+                Category:
+              </FormLabel>
+              <CheckboxGroup size="sm" colorScheme="yellow">
+                <Stack spacing={5} direction="row" fontFamily="font.body">
+                  <Checkbox>Meeting</Checkbox>
+                  <Checkbox>Election</Checkbox>
+                  <Checkbox>Inquiry</Checkbox>
+                  <Checkbox>Event</Checkbox>
+                  <Checkbox onChange={handleCheckboxChange}>
+                    New Category
+                  </Checkbox>
+                </Stack>
+                {/* New Category selected */}
+                {isNewCategorySelected && (
+                  <Input
+                    size="xs"
+                    type="string"
+                    placeholder="Enter New Category"
+                    p="1rem"
+                    mt="0.5rem"
+                    value={newCategory}
+                    onChange={(e) => setNewCategory(e.target.value)}
+                  />
+                )}
+              </CheckboxGroup>
+              <FormHelperText fontSize="xs" m="1">
+                Select the categories that apply to your post for members to
+                easily find it.
+              </FormHelperText>
+            </FormControl>
+            <Divider />
+
+            {/* Poll Question */}
+            <Box p="10px">
+              <Stack spacing="15px">
+                <FormControl isRequired>
+                  <FormLabel fontSize="sm" fontWeight="semibold">
+                    Poll Question:
+                  </FormLabel>
+                  <Input
+                    size="sm"
+                    type="string"
+                    placeholder="Enter a Poll Question"
+                  />
+                </FormControl>
+                <FormControl isRequired>
+                  <FormLabel fontSize="sm" fontWeight="semibold">
+                    Options:
+                  </FormLabel>
+                  {options.map((option, index) => (
+                    <HStack key={index} mt="0.5rem">
+                      <Input
+                        size="sm"
+                        type="string"
+                        placeholder={`Option ${index + 1}`}
+                        value={option}
+                        onChange={(e) => {
+                          const newOptions = [...options];
+                          newOptions[index] = e.target.value;
+                          setOptions(newOptions);
+                        }}
+                      />
+                      {index === options.length - 1 && (
+                        <Box alignSelf="center" ml="2%">
+                          <Button size="xs" w="20px" onClick={addOption}>
+                            <AddIcon />
+                          </Button>
+                        </Box>
+                      )}
+                    </HStack>
+                  ))}
+                </FormControl>
+              </Stack>
+            </Box>
+          </Stack>
+
+          <DialogFooter>
+            <Button
+              w="full"
+              size="sm"
+              colorScheme="yellow"
+              type="submit"
+              // onClick={() => onSubmit()}
+            >
+              Publish
+            </Button>
+          </DialogFooter>
+        </form>
       </DialogContent>
     </Dialog>
   );
--- app/(routes)/admin/community/_components/pollsAndSurveys/_create/surveys.tsx
+++ app/(routes)/admin/community/_components/pollsAndSurveys/_create/surveys.tsx
@@ -12,7 +12,6 @@ import {
 import {
   Input,
   Stack,
-  Text,
   Box,
   HStack,
   Spacer,
@@ -20,10 +19,44 @@ import {
   CheckboxGroup,
   Checkbox,
   Button,
+  FormControl,
+  FormLabel,
+  Textarea,
+  FormHelperText,
 } from "@chakra-ui/react";
 import { AddIcon } from "@chakra-ui/icons";
+import { useState } from "react";
 
 function createSurveys() {
+  let [surveyDescription, setSurveyDescription] = useState("");
+  let [options, setOptions] = useState([""]);
+  let [questions, setQuestions] = useState([{ question: "", options: [""] }]);
+
+  let handleSurveyDescriptionChange = (
+    e: React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    let inputSurveyDescription = e.target.value;
+    setSurveyDescription(inputSurveyDescription);
+  };
+
+  let [isNewCategorySelected, setIsNewCategorySelected] = useState(false);
+  let [newCategory, setNewCategory] = useState("");
+
+  let handleCheckboxChange = () => {
+    setIsNewCategorySelected(!isNewCategorySelected);
+    setNewCategory(""); // Clear the input field when toggling the checkbox
+  };
+
+  let addOption = (questionIndex: number) => {
+    const newQuestions = [...questions];
+    newQuestions[questionIndex].options.push("");
+    setQuestions(newQuestions);
+  };
+
+  let addQuestion = () => {
+    setQuestions([...questions, { question: "", options: [""] }]);
+  };
+
   return (
     <Dialog /*open={open} onOpenChange={setOpen}*/>
       <DialogTrigger asChild>
@@ -32,88 +65,158 @@ function createSurveys() {
           Create Survey
         </Button>
       </DialogTrigger>
-      <DialogContent className="">
-        <DialogHeader>
-          <DialogTitle>Create a Survey</DialogTitle>
-          <DialogDescription>
-            Fill up the following survey details.
-          </DialogDescription>
-        </DialogHeader>
+      <DialogContent className="lg:min-w-[800px]">
+        <form action="">
+          <DialogHeader>
+            <DialogTitle>Create a Survey</DialogTitle>
+            <DialogDescription>
+              Fill up the following fields to create a survey.
+            </DialogDescription>
+          </DialogHeader>
 
-        {/* Form Content */}
-        <Stack spacing="15px">
-          <Text fontSize="sm" fontWeight="semibold">
-            Survey Title:
-          </Text>
-          <Input size="sm" type="string" placeholder="Enter a Survey Title" />
-          <Text fontSize="sm" fontWeight="semibold">
-            Description:
-          </Text>
-          <Input size="sm" type="string" placeholder="Enter a Description" />
-
-          {/* Select Category */}
-          <Text fontSize="sm" fontWeight="semibold">
-            Category:
-          </Text>
-          <CheckboxGroup size="sm" colorScheme="yellow">
-            <Stack spacing={5} direction="row" fontFamily="font.body">
-              <Checkbox>Meeting</Checkbox>
-              <Checkbox>Election</Checkbox>
-              <Checkbox>Inquiry</Checkbox>
-              <Checkbox>Event</Checkbox>
-            </Stack>
-          </CheckboxGroup>
-          <Text fontSize="sm" fontWeight="semibold">
-            Add Category:
-          </Text>
-          <Input size="sm" type="string" placeholder="Enter a new category" />
-          <Divider />
-
-          {/* Survey Question */}
-          <Box p="10px">
-            <Stack spacing="15px">
-              <HStack>
-                <Text fontSize="sm" fontWeight="semibold">
-                  Survey Question:
-                </Text>
-                <Spacer />
-                <Box alignSelf="center" ml="2%">
-                  <Button size="xs" colorScheme="yellow">
-                    Add Question
-                  </Button>
-                </Box>
-              </HStack>
+          {/* Form Content */}
+          <Stack spacing="15px" my="2rem">
+            <FormControl isRequired>
+              <FormLabel fontSize="sm" fontWeight="semibold">
+                Survey Title:
+              </FormLabel>
               <Input
-                size="sm"
+                size="md"
+                fontWeight="semibold"
                 type="string"
-                placeholder="Enter a Survey Question"
+                placeholder="Enter a Survey Title"
               />
-              <Text fontSize="sm" fontWeight="semibold">
-                Options:
-              </Text>
-              <HStack>
-                <Input size="sm" type="string" placeholder="Option 1" />
-                <Box alignSelf="center" ml="2%">
-                  <Button size="xs" w="20px">
-                    <AddIcon />
-                  </Button>
-                </Box>
-              </HStack>
-            </Stack>
-          </Box>
-        </Stack>
+            </FormControl>
+            <FormControl isRequired>
+              <FormLabel fontSize="sm" fontWeight="semibold">
+                Description:
+              </FormLabel>
+              <Textarea
+                placeholder="Write something..."
+                id="surveyDescription"
+                fontSize="xs"
+                maxH="300px"
+                value={surveyDescription}
+                onChange={handleSurveyDescriptionChange}
+              />
+            </FormControl>
 
-        <DialogFooter>
-          <Button
-            w="full"
-            size="sm"
-            colorScheme="yellow"
-            type="submit"
-            // onClick={() => onSubmit()}
-          >
-            Publish
-          </Button>
-        </DialogFooter>
+            {/* Select Category */}
+            <FormControl isRequired>
+              <FormLabel fontSize="sm" fontWeight="semibold">
+                Category:
+              </FormLabel>
+              <CheckboxGroup size="sm" colorScheme="yellow">
+                <Stack spacing={5} direction="row" fontFamily="font.body">
+                  <Checkbox>Meeting</Checkbox>
+                  <Checkbox>Election</Checkbox>
+                  <Checkbox>Inquiry</Checkbox>
+                  <Checkbox>Event</Checkbox>
+                  <Checkbox onChange={handleCheckboxChange}>
+                    New Category
+                  </Checkbox>
+                </Stack>
+                {/* New Category selected */}
+                {isNewCategorySelected && (
+                  <Input
+                    size="xs"
+                    type="string"
+                    placeholder="Enter New Category"
+                    p="1rem"
+                    mt="0.5rem"
+                    value={newCategory}
+                    onChange={(e) => setNewCategory(e.target.value)}
+                  />
+                )}
+              </CheckboxGroup>
+              <FormHelperText fontSize="xs" m="1">
+                Select the categories that apply to your post for members to
+                easily find it.
+              </FormHelperText>
+            </FormControl>
+            <Divider />
+
+            {/* Survey Question */}
+            <Box p="10px" maxH="300px" overflowY="auto">
+              <Stack spacing="15px">
+                {questions.map((question, index) => (
+                  <FormControl key={index} isRequired>
+                    <HStack>
+                      <FormLabel fontSize="sm" fontWeight="semibold">
+                        Survey Question:
+                      </FormLabel>
+                      <Spacer />
+                      <Box alignSelf="center" ml="2%">
+                        <Button
+                          size="xs"
+                          colorScheme="yellow"
+                          onClick={addQuestion}
+                        >
+                          Add Question
+                        </Button>
+                      </Box>
+                    </HStack>
+                    <Input
+                      size="sm"
+                      type="string"
+                      placeholder="Enter a Survey Question"
+                      value={question.question}
+                      onChange={(e) => {
+                        const newQuestions = [...questions];
+                        newQuestions[index].question = e.target.value;
+                        setQuestions(newQuestions);
+                      }}
+                    />
+                    <FormControl isRequired>
+                      <FormLabel fontSize="sm" fontWeight="semibold">
+                        Options:
+                      </FormLabel>
+                      {question.options.map((option, optionIndex) => (
+                        <HStack key={optionIndex} mt="0.5rem">
+                          <Input
+                            size="sm"
+                            type="string"
+                            placeholder={`Option ${optionIndex + 1}`}
+                            value={option}
+                            onChange={(e) => {
+                              const newQuestions = [...questions];
+                              newQuestions[index].options[optionIndex] =
+                                e.target.value;
+                              setQuestions(newQuestions);
+                            }}
+                          />
+                          {optionIndex === question.options.length - 1 && (
+                            <Box alignSelf="center" ml="2%">
+                              <Button
+                                size="xs"
+                                w="20px"
+                                onClick={() => addOption(index)}
+                              >
+                                <AddIcon />
+                              </Button>
+                            </Box>
+                          )}
+                        </HStack>
+                      ))}
+                    </FormControl>
+                  </FormControl>
+                ))}
+              </Stack>
+            </Box>
+          </Stack>
+
+          <DialogFooter>
+            <Button
+              w="full"
+              size="sm"
+              colorScheme="yellow"
+              type="submit"
+              // onClick={() => onSubmit()}
+            >
+              Publish
+            </Button>
+          </DialogFooter>
+        </form>
       </DialogContent>
     </Dialog>
   );
