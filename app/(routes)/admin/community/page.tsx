c4dfc7a47c13a824734cc69c8213dbe25e1c937d
maxinefeliciano <a.maxine.feliciano@gmail.com>
2024-02-27 18:50:36 +0800

    Community Rules pop-up added

--- app/(routes)/admin/community/page.tsx
+++ app/(routes)/admin/community/page.tsx
@@ -5,7 +5,7 @@ import { Flex } from '@chakra-ui/react'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import DiscussionsCard from './_components/discussions/DiscussionsCard'
 import AnnouncementBoard from './_components/AnnouncementBoard'
-import HomeownerLinks from './_components/HomeownerLinks'
+import HomeownerResources from './_components/HomeownerResources'
 import AdminOfficerLinks from './_components/AdminOfficerLinks'
 import BusinessForumCard from './_components/business/BusinessForumCard'
 import PollsAndSurveysCard from './_components/pollsAndSurveys/PollsAndSurveysCard'
@@ -43,7 +43,7 @@ const Community = () => {
         </Tabs>
         <Flex flexDir='column' w='20vw' gap='2rem'>
           <AnnouncementBoard />
-          <HomeownerLinks />
+          <HomeownerResources />
           <AdminOfficerLinks />
         </Flex>
       </Flex>
