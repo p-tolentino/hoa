f422448f32e39bca7e0dcdda129bd36bf1c5ae99
kath <kathleen_tan@dlsu.edu.ph>
2024-02-27 22:37:59 +0800

    minor fixes for card height

--- app/(routes)/admin/community/_components/events/EventsCard.tsx
+++ app/(routes)/admin/community/_components/events/EventsCard.tsx
@@ -1,40 +1,40 @@
-'use client'
+"use client";
 
 import {
   Card,
   CardContent,
   CardDescription,
   CardHeader,
-  CardTitle
-} from '@/components/ui/card'
-import { Text, VStack } from '@chakra-ui/react'
-import { Calendar } from '@/components/ui/calendar'
-import React from 'react'
+  CardTitle,
+} from "@/components/ui/card";
+import { Text, VStack } from "@chakra-ui/react";
+import { Calendar } from "@/components/ui/calendar";
+import React from "react";
 
-export default function EventsCard () {
-  const [date, setDate] = React.useState<Date | undefined>(new Date())
+export default function EventsCard() {
+  const [date, setDate] = React.useState<Date | undefined>(new Date());
 
   return (
     <>
-      <Card className='h-[70vh]'>
+      <Card className="h-[75vh]">
         <CardHeader>
           <CardTitle>Events</CardTitle>
           <CardDescription>
             Create and view events within the HOA.
           </CardDescription>
         </CardHeader>
-        <CardContent className='space-y-2'>
-          <VStack h='500px' mt='3rem'>
-            <Text fontWeight='bold'>Google Calendar API here</Text>
+        <CardContent className="space-y-2">
+          <VStack h="500px" mt="3rem">
+            <Text fontWeight="bold">Google Calendar API here</Text>
             <Calendar
-              mode='single'
+              mode="single"
               selected={date}
               onSelect={setDate}
-              className='rounded-md border shadow'
+              className="rounded-md border shadow"
             />
           </VStack>
         </CardContent>
       </Card>
     </>
-  )
+  );
 }
