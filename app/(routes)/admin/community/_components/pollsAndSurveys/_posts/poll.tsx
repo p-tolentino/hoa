4b90c9b46c17e4d65618ac8f139a5dd07ab480ab
maxinefeliciano <a.maxine.feliciano@gmail.com>
2024-02-29 15:24:59 +0800

    Poll/Survey Status updated

--- app/(routes)/admin/community/_components/pollsAndSurveys/_posts/poll.tsx
+++ app/(routes)/admin/community/_components/pollsAndSurveys/_posts/poll.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client'
 
 import {
   Flex,
@@ -8,78 +8,71 @@ import {
   Heading,
   Avatar,
   HStack,
-  Spacer,
-  Button,
-} from "@chakra-ui/react";
-import AnswerPoll from "./_answer/poll";
-import PollResult from "./_result/poll";
-import { formatDistanceToNowStrict } from "date-fns";
+  Spacer
+} from '@chakra-ui/react'
+import AnswerPoll from './_answer/poll'
+import PollResult from './_result/poll'
+import { formatDistanceToNowStrict } from 'date-fns'
+import { useState } from 'react'
 
-function pollPosts() {
+function pollPosts () {
   const pollCategories = [
-    { category: "Meeting", color: "purple.200" },
-    { category: "Election", color: "pink.200" },
-    { category: "Inquiry", color: "blue.200" },
-    { category: "Event", color: "orange.200" },
-  ];
+    { category: 'Meeting', color: 'purple.200' },
+    { category: 'Election', color: 'pink.200' },
+    { category: 'Inquiry', color: 'blue.200' },
+    { category: 'Event', color: 'orange.200' }
+  ]
 
-  const datePosted = new Date(2024, 2, 1);
-  const dateDistance = formatDistanceToNowStrict(datePosted);
+  const [postStatus, setPostStatus] = useState('Closed')
+
+  const datePosted = new Date(2024, 2, 1)
+  const dateDistance = formatDistanceToNowStrict(datePosted)
 
   return (
-    <Flex p="10px">
+    <Flex p='10px'>
       <Box
-        w="100%"
-        h="100%"
-        border="1px"
-        borderColor="gray.200"
-        borderRadius="10px"
-        mb="1%"
+        w='100%'
+        h='100%'
+        border='1px'
+        borderColor='gray.200'
+        borderRadius='10px'
+        mb='1%'
       >
-        {/* <Box
-          bgColor="green.200"
-          fontSize="xs"
-          fontWeight="bold"
-          w="5%"
-          textAlign="center"
-          ml="20px"
-        >
-          Open
-        </Box> */}
-        <Box
-          bgColor="red.200"
-          fontSize="xs"
-          fontWeight="semibold"
-          w="6%"
-          textAlign="center"
-          ml="20px"
-        >
-          Closed
-        </Box>
-
-        <Box p="20px">
-          <HStack>
-            <Heading size="md" fontFamily="font.heading" mb="1%">
+        <Box p='20px'>
+          <HStack spacing={1}>
+            {/* Survey Title */}
+            <Heading size='md' fontFamily='font.heading' mb='1%'>
               Poll Title
             </Heading>
+
+            {/* Survey Status */}
+            <Text
+              as='sup'
+              fontWeight='bold'
+              color={postStatus === 'Open' ? 'green.500' : 'red.500'}
+            >
+              ({postStatus})
+            </Text>
+
             <Spacer />
-            {/* <AnswerPoll /> */}
-            <PollResult />
+
+            {/* Survey Button */}
+            {postStatus === 'Open' ? <AnswerPoll /> : <PollResult />}
           </HStack>
 
-          {/* Poll Catergories */}
-          <HStack mb="2%">
+          {/* Poll Categories */}
+          <HStack mb='2%'>
             {pollCategories.map((pollCategory, index) => (
               <Box
                 key={index}
                 bg={pollCategory.color}
-                fontFamily="font.heading"
-                fontSize="xs"
-                fontWeight="semibold"
-                w="10%"
-                p="3px"
-                textAlign="center"
-                rounded="md"
+                fontFamily='font.heading'
+                fontSize='xs'
+                fontWeight='semibold'
+                w='10%'
+                p='3px'
+                textAlign='center'
+                rounded='md'
               >
                 {pollCategory.category}
               </Box>
@@ -87,33 +80,33 @@ function pollPosts() {
           </HStack>
 
           {/* Poll Details */}
-          <HStack p="5px">
+          <HStack p='5px'>
             <Avatar /> {/*default size is medium*/}
-            <Stack spacing="0.5px">
+            <Stack spacing='0.5px'>
               <Text
-                id="name"
-                fontSize="sm"
-                fontWeight="bold"
-                fontFamily="font.body"
+                id='name'
+                fontSize='sm'
+                fontWeight='bold'
+                fontFamily='font.body'
               >
                 Name
               </Text>
               <Text
-                id="position"
-                fontSize="sm"
-                fontWeight="bold"
-                fontFamily="font.body"
+                id='position'
+                fontSize='sm'
+                fontWeight='bold'
+                fontFamily='font.body'
               >
                 Position (Homeowner or Officer)
               </Text>
             </Stack>
           </HStack>
           <Text
-            id="description"
-            ml="5.5%"
-            fontSize="sm"
-            p="5px"
-            fontFamily="font.body"
+            id='description'
+            ml='5.5%'
+            fontSize='sm'
+            p='5px'
+            fontFamily='font.body'
           >
             Description of Poll Description of Poll Description of Poll
             Description of Poll Description of Poll Description of Poll
@@ -122,17 +115,17 @@ function pollPosts() {
           </Text>
           {/* Date distance */}
           <Text
-            fontFamily="font.body"
-            color="grey"
-            fontSize="xs"
-            ml="5.5%"
-            p="5px"
+            fontFamily='font.body'
+            color='grey'
+            fontSize='xs'
+            ml='5.5%'
+            p='5px'
           >
             Posted {dateDistance} ago
           </Text>
         </Box>
       </Box>
     </Flex>
-  );
+  )
 }
-export default pollPosts;
+export default pollPosts
--- app/(routes)/admin/community/_components/pollsAndSurveys/_posts/survey.tsx
+++ app/(routes)/admin/community/_components/pollsAndSurveys/_posts/survey.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client'
 
 import {
   Flex,
@@ -8,79 +8,71 @@ import {
   Heading,
   Avatar,
   HStack,
-  Spacer,
-  Button,
-} from "@chakra-ui/react";
-import AnswerSurvey from "./_answer/survey";
-import SurveyResult from "./_result/survey";
-import { formatDistanceToNowStrict } from "date-fns";
+  Spacer
+} from '@chakra-ui/react'
+import AnswerSurvey from './_answer/survey'
+import SurveyResult from './_result/survey'
+import { formatDistanceToNowStrict } from 'date-fns'
+import { useState } from 'react'
 
-function surveyPosts() {
+function surveyPosts () {
   const pollCategories = [
-    { category: "Meeting", color: "purple.200" },
-    { category: "Election", color: "pink.200" },
-    { category: "Inquiry", color: "blue.200" },
-    { category: "Event", color: "orange.200" },
-    { category: "New Category", color: "teal.200" },
-  ];
+    { category: 'Meeting', color: 'purple.200' },
+    { category: 'Election', color: 'pink.200' },
+    { category: 'Inquiry', color: 'blue.200' },
+    { category: 'Event', color: 'orange.200' },
+    { category: 'New Category', color: 'teal.200' }
+  ]
 
-  const datePosted = new Date(2024, 2, 1);
-  const dateDistance = formatDistanceToNowStrict(datePosted);
+  const [postStatus, setPostStatus] = useState('Open')
+
+  const datePosted = new Date(2024, 2, 1)
+  const dateDistance = formatDistanceToNowStrict(datePosted)
 
   return (
-    <Flex p="10px">
+    <Flex p='10px'>
       <Box
-        w="100%"
-        h="100%"
-        border="1px"
-        borderColor="gray.200"
-        borderRadius="10px"
-        mb="1%"
+        w='100%'
+        h='100%'
+        border='1px'
+        borderColor='gray.200'
+        borderRadius='10px'
+        mb='1%'
       >
-        <Box
-          bgColor="green.200"
-          fontSize="xs"
-          fontWeight="bold"
-          w="5%"
-          textAlign="center"
-          ml="20px"
-        >
-          Open
-        </Box>
-        {/* <Box
-          bgColor="red.200"
-          fontSize="xs"
-          fontWeight="semibold"
-          w="6%"
-          textAlign="center"
-          ml="20px"
-        >
-          Closed
-        </Box> */}
-
-        <Box p="20px">
-          <HStack>
-            <Heading size="md" fontFamily="font.heading" mb="1%">
+        <Box p='20px'>
+          <HStack spacing={1}>
+            {/* Survey Title */}
+            <Heading size='md' fontFamily='font.heading' mb='1%'>
               Survey Title
             </Heading>
+
+            {/* Survey Status */}
+            <Text
+              as='sup'
+              fontWeight='bold'
+              color={postStatus === 'Open' ? 'green.500' : 'red.500'}
+            >
+              ({postStatus})
+            </Text>
             <Spacer />
-            {/* <AnswerSurvey /> */}
-            <SurveyResult />
+
+            {/* Survey Button */}
+            {postStatus === 'Open' ? <AnswerSurvey /> : <SurveyResult />}
           </HStack>
 
-          {/* Survey Catergories */}
-          <HStack mb="2%">
+          {/* Survey Categories */}
+          <HStack mb='2%'>
             {pollCategories.map((pollCategory, index) => (
               <Box
                 key={index}
                 bg={pollCategory.color}
-                fontFamily="font.heading"
-                fontSize="xs"
-                fontWeight="semibold"
-                w="10%"
-                p="3px"
-                textAlign="center"
-                rounded="md"
+                fontFamily='font.heading'
+                fontSize='xs'
+                fontWeight='semibold'
+                w='10%'
+                p='3px'
+                textAlign='center'
+                rounded='md'
               >
                 {pollCategory.category}
               </Box>
@@ -88,33 +80,33 @@ function surveyPosts() {
           </HStack>
 
           {/* Survey Details */}
-          <HStack p="5px">
+          <HStack p='5px'>
             <Avatar /> {/*default size is medium*/}
-            <Stack spacing="0.5px">
+            <Stack spacing='0.5px'>
               <Text
-                id="name"
-                fontSize="sm"
-                fontWeight="bold"
-                fontFamily="font.body"
+                id='name'
+                fontSize='sm'
+                fontWeight='bold'
+                fontFamily='font.body'
               >
                 Name
               </Text>
               <Text
-                id="position"
-                fontSize="sm"
-                fontWeight="bold"
-                fontFamily="font.body"
+                id='position'
+                fontSize='sm'
+                fontWeight='bold'
+                fontFamily='font.body'
               >
                 Position (Homeowner or Officer)
               </Text>
             </Stack>
           </HStack>
           <Text
-            id="description"
-            ml="5.5%"
-            fontSize="sm"
-            p="5px"
-            fontFamily="font.body"
+            id='description'
+            ml='5.5%'
+            fontSize='sm'
+            p='5px'
+            fontFamily='font.body'
           >
             Description of Survey Description of Survey Description of Survey
             Description of Survey Description of Survey Description of Survey
@@ -122,17 +114,17 @@ function surveyPosts() {
           </Text>
           {/* Date distance */}
           <Text
-            fontFamily="font.body"
-            color="grey"
-            fontSize="xs"
-            ml="5.5%"
-            p="5px"
+            fontFamily='font.body'
+            color='grey'
+            fontSize='xs'
+            ml='5.5%'
+            p='5px'
           >
             Posted {dateDistance} ago
           </Text>
         </Box>
       </Box>
     </Flex>
-  );
+  )
 }
-export default surveyPosts;
+export default surveyPosts
