8e72199f7a7c7739868a68ad89a5f502479ecd28
kath <kathleen_tan@dlsu.edu.ph>
2024-03-04 19:21:29 +0800

    added a remove button for the Person Involved section

--- app/(routes)/admin/disputes/complaint-form/page.tsx
+++ app/(routes)/admin/disputes/complaint-form/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import {
   FormControl,
@@ -10,115 +10,151 @@ import {
   Box,
   Stack,
   RadioGroup,
-  Radio
-} from '@chakra-ui/react'
-import { useState } from 'react'
-import { Separator } from '@/components/ui/separator'
-import { Heading } from '@/components/ui/heading'
-import { AddIcon } from '@chakra-ui/icons'
+  Radio,
+  HStack,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import { Separator } from "@/components/ui/separator";
+import { Heading } from "@/components/ui/heading";
+import { AddIcon, DeleteIcon } from "@chakra-ui/icons";
 
-export default function FileComplaint () {
-  const title = 'File a Complaint'
+export default function FileComplaint() {
+  const title = "File a Complaint";
   const description =
-    "Fill out the Complaint Form to formally request for a dispute resolution from the Homeowners' Association."
+    "Fill out the Complaint Form to formally request for a dispute resolution from the Homeowners' Association.";
 
-  const [type, setType] = useState('')
-  const [personsInvolved, setPersonsInvolved] = useState([''])
+  const [type, setType] = useState("");
+  const [personsInvolved, setPersonsInvolved] = useState([""]);
 
   const addPersonInput = () => {
-    setPersonsInvolved([...personsInvolved, ''])
-  }
+    setPersonsInvolved([...personsInvolved, ""]);
+  };
+
+  const removePersonInput = (index: number) => {
+    const updatedPersonsInvolved = [...personsInvolved];
+    updatedPersonsInvolved.splice(index, 1);
+    setPersonsInvolved(updatedPersonsInvolved);
+  };
 
   const handlePersonInputChange = (index: number, value: string) => {
-    const updatedPersonsInvolved = [...personsInvolved]
-    updatedPersonsInvolved[index] = value
-    setPersonsInvolved(updatedPersonsInvolved)
-  }
+    const updatedPersonsInvolved = [...personsInvolved];
+    updatedPersonsInvolved[index] = value;
+    setPersonsInvolved(updatedPersonsInvolved);
+  };
 
   return (
     <>
       <Heading title={title} description={description} />
-      <Separator className='mt-4 mb-6' />
+      <Separator className="mt-4 mb-6" />
 
       <Box
-        w='60%'
-        border='1px'
-        borderColor='gray.200'
-        borderRadius='10px'
-        mt='2%'
-        p='20px'
+        w="60%"
+        border="1px"
+        borderColor="gray.200"
+        borderRadius="10px"
+        mt="2%"
+        p="20px"
       >
         <Stack spacing={5}>
           {/* Complaint Title */}
           <FormControl isRequired>
-            <FormLabel fontSize='md' fontFamily='font.body'>
+            <FormLabel fontSize="md" fontFamily="font.body">
               Title of Complaint
             </FormLabel>
-            <Input w='50%' size='sm' type='string' fontWeight='semibold' />
+            <Input
+              w="50%"
+              size="sm"
+              type="string"
+              fontWeight="semibold"
+              placeholder="Enter a Title"
+            />
           </FormControl>
 
           {/* Complaint Type */}
           <FormControl isRequired>
-            <FormLabel fontSize='sm' fontWeight='semibold'>
+            <FormLabel fontSize="sm" fontWeight="semibold">
               Complaint Type
             </FormLabel>
             <RadioGroup
-              size='sm'
-              colorScheme='yellow'
+              size="sm"
+              colorScheme="yellow"
               onChange={setType}
               value={type}
             >
-              <Stack spacing={5} direction='row' fontFamily='font.body'>
-                <Radio value='parking'>Parking</Radio>
-                <Radio value='noise'>Noise and Nuisance</Radio>
-                <Radio value='behavior'>Behavioral Issues</Radio>
-                <Radio value='pet'>Pet-related Issues</Radio>
-                <Radio value='other'>Other</Radio>
+              <Stack spacing={5} direction="row" fontFamily="font.body">
+                <Radio value="parking">Parking</Radio>
+                <Radio value="noise">Noise and Nuisance</Radio>
+                <Radio value="behavior">Behavioral Issues</Radio>
+                <Radio value="pet">Pet-related Issues</Radio>
+                <Radio value="other">Other</Radio>
               </Stack>
             </RadioGroup>
 
             {/* Complaint */}
           </FormControl>
           <FormControl isRequired>
-            <FormLabel fontSize='md' fontFamily='font.body'>
+            <FormLabel fontSize="md" fontFamily="font.body">
               Complaint
             </FormLabel>
             <Textarea
-              size='sm'
-              placeholder='Tell us what happened...'
-              h='200px'
-              resize={'none'}
+              size="sm"
+              placeholder="Tell us what happened..."
+              h="200px"
+              fontFamily="font.body"
+              resize={"none"}
             />
           </FormControl>
 
           {/* Person/s Involved */}
           <FormControl isRequired>
-            <FormLabel fontSize='md' fontFamily='font.body'>
-              Person/s Involved
-            </FormLabel>
+            <HStack justifyContent="space-between">
+              <FormLabel fontSize="md" fontFamily="font.body">
+                Person/s Involved
+              </FormLabel>
+              <Button
+                size="xs"
+                mt="-1"
+                leftIcon={<AddIcon />}
+                onClick={addPersonInput}
+              >
+                Add Person
+              </Button>
+            </HStack>
             {personsInvolved.map((person, index) => (
-              <Input
-                key={index}
-                size='sm'
-                type='string'
-                mb={2}
-                value={person}
-                onChange={e => handlePersonInputChange(index, e.target.value)}
-              />
+              <Box key={index} display="flex" alignItems="center">
+                <Input
+                  key={index}
+                  size="sm"
+                  type="string"
+                  placeholder="Enter a Name"
+                  mb={2}
+                  value={person}
+                  onChange={(e) =>
+                    handlePersonInputChange(index, e.target.value)
+                  }
+                />
+                {personsInvolved.length > 1 && index !== 0 && (
+                  <Button
+                    size="xs"
+                    colorScheme="red"
+                    ml={2}
+                    onClick={() => removePersonInput(index)}
+                  >
+                    Remove
+                  </Button>
+                )}
+              </Box>
             ))}
-            <Button size='xs' leftIcon={<AddIcon />} onClick={addPersonInput}>
-              Add Person
-            </Button>
-            <FormHelperText fontSize='xs'>
+            <FormHelperText fontSize="xs">
               This will allow us to contact the individuals involved in the
               complaint.
             </FormHelperText>
           </FormControl>
 
           {/* Submit Button */}
-          <Box textAlign='center'>
+          <Box textAlign="center">
             <FormControl>
-              <Button size='sm' type='submit' colorScheme='yellow' my='20px'>
+              <Button size="sm" type="submit" colorScheme="yellow" my="20px">
                 Submit Complaint
               </Button>
             </FormControl>
@@ -126,5 +162,5 @@ export default function FileComplaint () {
         </Stack>
       </Box>
     </>
-  )
+  );
 }
