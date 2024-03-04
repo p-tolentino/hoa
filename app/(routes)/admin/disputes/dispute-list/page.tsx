44bb45a5a478f5e9ef1c6d18158883630862c892
kath <kathleen_tan@dlsu.edu.ph>
2024-03-05 03:20:23 +0800

    made the cards in dispute-list scrollable

--- app/(routes)/admin/disputes/dispute-list/page.tsx
+++ app/(routes)/admin/disputes/dispute-list/page.tsx
@@ -7,6 +7,7 @@ import {
   Text,
   SimpleGrid,
   Flex,
+  Box,
   HStack,
   Card,
   CardHeader,
@@ -175,119 +176,125 @@ export default function ListOfDisputes() {
         </Stack>
       </Flex>
       <Separator className="mt-4 mb-6" />
-      <SimpleGrid columns={3} spacing={5} px={2}>
-        {disputes.map((dispute) => (
-          <div>
-            <Card key={dispute.title} pb={3}>
-              <Stack>
-                <CardHeader pb="0">
-                  <HStack justifyContent="space-between" align="end">
-                    {/* Dispute Title */}
-                    <Text size="md" fontWeight="bold" fontFamily="font.heading">
-                      {dispute.title}
-                    </Text>
-                    <ButtonGroup>
-                      {/* Edit Dispute Button */}
-                      <EditDisputeButton
-                        key={dispute.title}
-                        title={dispute.title}
-                        description={dispute.description}
-                        fees={dispute.fees}
-                      />
-
-                      {/* Delete Dispute Button */}
-                      <Button
-                        key={dispute.title}
-                        size="sm"
-                        mr="10px"
-                        colorScheme="red"
-                        onClick={() => removeDispute(dispute.title)}
+      <Box w="100%" h="75vh" overflowY="auto">
+        <SimpleGrid columns={3} spacing={5} px={2}>
+          {disputes.map((dispute) => (
+            <div>
+              <Card key={dispute.title} pb={3}>
+                <Stack>
+                  <CardHeader pb="0">
+                    <HStack justifyContent="space-between" align="end">
+                      {/* Dispute Title */}
+                      <Text
+                        size="md"
+                        fontWeight="bold"
+                        fontFamily="font.heading"
                       >
-                        <DeleteIcon />
-                      </Button>
-                    </ButtonGroup>
-                  </HStack>
-                </CardHeader>
-                <CardBody pt={3} minH="100px">
-                  {/* Dispute Description */}
-                  <Text
-                    fontSize="sm"
-                    fontFamily="font.body"
-                    textAlign="justify"
-                  >
-                    {dispute.description}
-                  </Text>
-                </CardBody>
-              </Stack>
-
-              {/* View Possible Fees */}
-              <Accordion allowToggle>
-                <AccordionItem key={dispute.title} border="0" m="0" p="0">
-                  <AccordionButton
-                    key={dispute.title}
-                    justifyContent="space-between"
-                  >
-                    <Text fontSize="sm" fontFamily="font.body" as="u">
-                      View possible fees
-                    </Text>
-                    <AccordionIcon />
-                  </AccordionButton>
-                  <AccordionPanel
-                    key={dispute.title}
-                    mx={4}
-                    px={4}
-                    pt={2}
-                    pb={5}
-                    borderRadius={5}
-                  >
-                    {/* Fees Table */}
-                    {dispute.fees ? (
-                      <TableContainer>
-                        <Table
-                          size="xs"
-                          w="300px"
-                          key={dispute.title}
-                          fontFamily="font.body"
-                        >
-                          <Thead>
-                            <Tr>
-                              <Th fontSize="xs" fontFamily="font.body">
-                                Fees
-                              </Th>
-                              <Th />
-                            </Tr>
-                          </Thead>
-                          <Tbody fontSize="sm" fontFamily="font.body">
-                            {dispute.fees.map((fee, index) => (
-                              <Tr key={index}>
-                                <Td>{fee.name}</Td>
-                                <Td
-                                  isNumeric
-                                  color={
-                                    fee.cost !== "Cost may vary"
-                                      ? "black"
-                                      : "lightgrey"
-                                  }
-                                >
-                                  {fee.cost}
-                                </Td>
-                              </Tr>
-                            ))}
-                          </Tbody>
-                        </Table>
-                      </TableContainer>
-                    ) : (
-                      <Text fontSize="sm" fontFamily="font.body" color="grey">
-                        No fees available for this dispute.
+                        {dispute.title}
                       </Text>
-                    )}
-                  </AccordionPanel>
-                </AccordionItem>
-              </Accordion>
-            </Card>
-          </div>
-        ))}
-      </SimpleGrid>
+                      <ButtonGroup>
+                        {/* Edit Dispute Button */}
+                        <EditDisputeButton
+                          key={dispute.title}
+                          title={dispute.title}
+                          description={dispute.description}
+                          fees={dispute.fees}
+                        />
+
+                        {/* Delete Dispute Button */}
+                        <Button
+                          key={dispute.title}
+                          size="sm"
+                          mr="10px"
+                          colorScheme="red"
+                          onClick={() => removeDispute(dispute.title)}
+                        >
+                          <DeleteIcon />
+                        </Button>
+                      </ButtonGroup>
+                    </HStack>
+                  </CardHeader>
+                  <CardBody pt={3} minH="100px">
+                    {/* Dispute Description */}
+                    <Text
+                      fontSize="sm"
+                      fontFamily="font.body"
+                      textAlign="justify"
+                    >
+                      {dispute.description}
+                    </Text>
+                  </CardBody>
+                </Stack>
+
+                {/* View Possible Fees */}
+                <Accordion allowToggle>
+                  <AccordionItem key={dispute.title} border="0" m="0" p="0">
+                    <AccordionButton
+                      key={dispute.title}
+                      justifyContent="space-between"
+                    >
+                      <Text fontSize="sm" fontFamily="font.body" as="u">
+                        View possible fees
+                      </Text>
+                      <AccordionIcon />
+                    </AccordionButton>
+                    <AccordionPanel
+                      key={dispute.title}
+                      mx={4}
+                      px={4}
+                      pt={2}
+                      pb={5}
+                      borderRadius={5}
+                    >
+                      {/* Fees Table */}
+                      {dispute.fees ? (
+                        <TableContainer>
+                          <Table
+                            size="xs"
+                            w="300px"
+                            key={dispute.title}
+                            fontFamily="font.body"
+                          >
+                            <Thead>
+                              <Tr>
+                                <Th fontSize="xs" fontFamily="font.body">
+                                  Fees
+                                </Th>
+                                <Th />
+                              </Tr>
+                            </Thead>
+                            <Tbody fontSize="sm" fontFamily="font.body">
+                              {dispute.fees.map((fee, index) => (
+                                <Tr key={index}>
+                                  <Td>{fee.name}</Td>
+                                  <Td
+                                    isNumeric
+                                    color={
+                                      fee.cost !== "Cost may vary"
+                                        ? "black"
+                                        : "lightgrey"
+                                    }
+                                  >
+                                    {fee.cost}
+                                  </Td>
+                                </Tr>
+                              ))}
+                            </Tbody>
+                          </Table>
+                        </TableContainer>
+                      ) : (
+                        <Text fontSize="sm" fontFamily="font.body" color="grey">
+                          No fees available for this dispute.
+                        </Text>
+                      )}
+                    </AccordionPanel>
+                  </AccordionItem>
+                </Accordion>
+              </Card>
+            </div>
+          ))}
+        </SimpleGrid>
+      </Box>
     </>
   );
 }
