4b71202dfe88370c79c2fcec34b431b549f4baee
kath <kathleen_tan@dlsu.edu.ph>
2024-03-04 19:20:28 +0800

    delete button working + justified the description text

--- app/(routes)/admin/disputes/dispute-list/page.tsx
+++ app/(routes)/admin/disputes/dispute-list/page.tsx
@@ -1,7 +1,7 @@
-'use client'
+"use client";
 
-import { Heading } from '@/components/ui/heading'
-import { Separator } from '@/components/ui/separator'
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
 import {
   Stack,
   Text,
@@ -12,52 +12,62 @@ import {
   CardHeader,
   CardBody,
   Button,
-  ButtonGroup
-} from '@chakra-ui/react'
-import AddDisputeButton from './_components/AddDisputeButton'
-import EditDisputeButton from './_components/EditDisputeButton'
-import { DeleteIcon } from '@chakra-ui/icons'
+  ButtonGroup,
+} from "@chakra-ui/react";
+import AddDisputeButton from "./_components/AddDisputeButton";
+import EditDisputeButton from "./_components/EditDisputeButton";
+import { DeleteIcon } from "@chakra-ui/icons";
+import * as React from "react";
 
-export default function ListOfDisputes () {
-  const title = 'List of Common Association Disputes'
+export default function ListOfDisputes() {
+  const title = "List of Common Association Disputes";
   const description =
-    'View the list of common disputes occurence in a homeowners association.'
+    "View the list of common disputes occurence in a homeowners association.";
   const commonDisputes = [
     {
-      title: 'Parking Disputes',
+      title: "Parking Disputes",
       description:
-        'Common parking disputes include issues with unauthorized vehicles, improper use of designated parking spaces, or disputes over the number of vehicles a homeowner is allowed to park within the community.'
+        "Common parking disputes include issues with unauthorized vehicles, improper use of designated parking spaces, or disputes over the number of vehicles a homeowner is allowed to park within the community.",
     },
     {
-      title: 'Noise and Nuisance',
+      title: "Noise and Nuisance",
       description:
-        'Issues related to excessive noise, disruptive behavior, or activities that create a nuisance can lead to disputes. This may involve conflicts between neighbors or enforcement of community rules to maintain a peaceful living environment.'
+        "Issues related to excessive noise, disruptive behavior, or activities that create a nuisance can lead to disputes. This may involve conflicts between neighbors or enforcement of community rules to maintain a peaceful living environment.",
     },
     {
-      title: 'Behavioral Issues',
+      title: "Behavioral Issues",
       description:
-        'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam alias sequi suscipit voluptas, corporis nemo.'
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam alias sequi suscipit voluptas, corporis nemo.",
     },
     {
-      title: 'Pet-related Issues',
+      title: "Pet-related Issues",
       description:
-        "Conflicts may arise over pet ownership, noise from pets, failure to adhere to leash rules, or violations of pet-related guidelines outlined in the HOA's governing documents."
-    }
-  ]
+        "Conflicts may arise over pet ownership, noise from pets, failure to adhere to leash rules, or violations of pet-related guidelines outlined in the HOA's governing documents.",
+    },
+  ];
+
+  const [disputes, setDisputes] = React.useState([...commonDisputes]);
+
+  const removeDispute = (titleToRemove: string) => {
+    const updatedDisputes = disputes.filter(
+      (dispute) => dispute.title !== titleToRemove
+    );
+    setDisputes(updatedDisputes);
+  };
 
   return (
     <>
-      <Flex justifyContent='space-between'>
+      <Flex justifyContent="space-between">
         <Heading title={title} description={description} />
         <AddDisputeButton />
       </Flex>
-      <Separator className='mt-4 mb-6' />
+      <Separator className="mt-4 mb-6" />
       <SimpleGrid columns={3} spacing={5} px={2}>
-        {commonDisputes.map(dispute => (
+        {disputes.map((dispute) => (
           <Card key={dispute.title} pb={3}>
-            <CardHeader pb='0'>
-              <HStack justifyContent='space-between' align='end'>
-                <Text size='md' fontWeight='bold' fontFamily='font.heading'>
+            <CardHeader pb="0">
+              <HStack justifyContent="space-between" align="end">
+                <Text size="md" fontWeight="bold" fontFamily="font.heading">
                   {dispute.title}
                 </Text>
                 <ButtonGroup>
@@ -66,14 +76,19 @@ export default function ListOfDisputes () {
                     title={dispute.title}
                     description={dispute.description}
                   />
-                  <Button size='sm' mr='10px' colorScheme='red'>
+                  <Button
+                    size="sm"
+                    mr="10px"
+                    colorScheme="red"
+                    onClick={() => removeDispute(dispute.title)}
+                  >
                     <DeleteIcon />
                   </Button>
                 </ButtonGroup>
               </HStack>
             </CardHeader>
             <CardBody pt={3}>
-              <Text fontSize='sm' fontFamily='font.body'>
+              <Text fontSize="sm" fontFamily="font.body" textAlign="justify">
                 {dispute.description}
               </Text>
             </CardBody>
@@ -81,5 +96,5 @@ export default function ListOfDisputes () {
         ))}
       </SimpleGrid>
     </>
-  )
+  );
 }
