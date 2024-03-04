ce14ff216c5bbc424d4fc11c34f9201e57706ad0
kath <kathleen_tan@dlsu.edu.ph>
2024-03-05 02:27:02 +0800

    added fees table in add and edit dispute buttons

--- app/(routes)/admin/disputes/dispute-list/_components/AddDisputeButton.tsx
+++ app/(routes)/admin/disputes/dispute-list/_components/AddDisputeButton.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import {
   Dialog,
@@ -7,29 +7,63 @@ import {
   DialogFooter,
   DialogHeader,
   DialogTitle,
-  DialogTrigger
-} from '@/components/ui/dialog'
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import {
   Input,
   Stack,
   Button,
   FormControl,
   FormLabel,
-  Textarea
-} from '@chakra-ui/react'
-import { AddIcon } from '@chakra-ui/icons'
+  Textarea,
+  TableContainer,
+  Table,
+  Thead,
+  Tr,
+  Th,
+  Tbody,
+  Td,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import { AddIcon, DeleteIcon } from "@chakra-ui/icons";
 
-export default function AddDispute () {
+export default function AddDispute() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [feesList, setFeesList] = useState([{ name: "", cost: "" }]);
+
+  const handleAddFee = () => {
+    setFeesList([...feesList, { name: "", cost: "" }]);
+  };
+
+  const handleRemoveFee = (index: number) => {
+    const updatedFeesList = [...feesList];
+    updatedFeesList.splice(index, 1);
+    setFeesList(updatedFeesList);
+  };
+
+  const handleFeeNameChange = (index: number, value: string) => {
+    const updatedFeesList = [...feesList];
+    updatedFeesList[index].name = value;
+    setFeesList(updatedFeesList);
+  };
+
+  const handleCostChange = (index: number, value: string) => {
+    const updatedFeesList = [...feesList];
+    updatedFeesList[index].cost = value;
+    setFeesList(updatedFeesList);
+  };
+
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <Button size='sm' colorScheme='yellow'>
-          <AddIcon boxSize={3} mr='10px' />
+        <Button size="sm" colorScheme="yellow">
+          <AddIcon boxSize={3} mr="10px" />
           Add Dispute
         </Button>
       </DialogTrigger>
-      <DialogContent className='lg:min-w-[800px]'>
-        <form action=''>
+      <DialogContent className="lg:min-w-[800px]">
+        <form action="">
           <DialogHeader>
             <DialogTitle>Add a Dispute</DialogTitle>
             <DialogDescription>
@@ -39,40 +73,103 @@ export default function AddDispute () {
           </DialogHeader>
 
           {/* Form Content */}
-          <Stack spacing='15px' my='2rem'>
+          <Stack spacing="15px" my="2rem">
             {/* Dispute Title */}
             <FormControl isRequired>
-              <FormLabel fontSize='sm' fontWeight='semibold'>
+              <FormLabel fontSize="sm" fontWeight="semibold">
                 Dispute Title:
               </FormLabel>
               <Input
-                size='md'
-                fontWeight='semibold'
-                type='string'
-                placeholder='Enter a Title'
+                size="md"
+                fontWeight="semibold"
+                type="string"
+                placeholder="Enter a Title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
               />
             </FormControl>
 
             {/* Dispute Description */}
             <FormControl isRequired>
-              <FormLabel fontSize='sm' fontWeight='semibold'>
+              <FormLabel fontSize="sm" fontWeight="semibold">
                 Description:
               </FormLabel>
               <Textarea
-                placeholder='Write something...'
-                fontSize='sm'
-                resize='none'
+                fontFamily="font.body"
+                placeholder="Write something..."
+                fontSize="sm"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                resize="none"
               />
             </FormControl>
+
+            {/* Set Possible Fees and Costs*/}
+            <TableContainer>
+              <Table size="xs" w="80%" fontFamily="font.body">
+                <Thead>
+                  <Tr>
+                    <Th fontSize="xs" fontFamily="font.body">
+                      Fees
+                    </Th>
+                    <Th fontSize="xs" fontFamily="font.body">
+                      Costs
+                    </Th>
+                    <Th>
+                      <Button size="xs" onClick={handleAddFee}>
+                        <AddIcon />
+                      </Button>
+                    </Th>
+                  </Tr>
+                </Thead>
+                <Tbody fontSize="sm" fontFamily="font.body">
+                  {feesList.map((fee, index) => (
+                    <Tr key={index}>
+                      <Td>
+                        <Input
+                          size="sm"
+                          placeholder="Enter a Fee Name"
+                          value={fee.name}
+                          onChange={(e) =>
+                            handleFeeNameChange(index, e.target.value)
+                          }
+                        />
+                      </Td>
+                      <Td>
+                        <Input
+                          size="sm"
+                          placeholder="Enter a Cost"
+                          value={fee.cost}
+                          onChange={(e) =>
+                            handleCostChange(index, e.target.value)
+                          }
+                        />
+                      </Td>
+                      <Td>
+                        {feesList.length > 1 && index !== 0 && (
+                          <Button
+                            size="xs"
+                            colorScheme="red"
+                            onClick={() => handleRemoveFee(index)}
+                          >
+                            <DeleteIcon />
+                          </Button>
+                        )}
+                      </Td>
+                    </Tr>
+                  ))}
+                </Tbody>
+              </Table>
+            </TableContainer>
           </Stack>
 
           <DialogFooter>
-            <Button size='sm' colorScheme='yellow' type='submit'>
+            <Button size="sm" colorScheme="yellow" type="submit">
               Add Dispute
             </Button>
           </DialogFooter>
         </form>
       </DialogContent>
     </Dialog>
-  )
+  );
 }
--- app/(routes)/admin/disputes/dispute-list/_components/EditDisputeButton.tsx
+++ app/(routes)/admin/disputes/dispute-list/_components/EditDisputeButton.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import {
   Dialog,
@@ -7,34 +7,71 @@ import {
   DialogFooter,
   DialogHeader,
   DialogTitle,
-  DialogTrigger
-} from '@/components/ui/dialog'
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import {
   Input,
   Stack,
   Button,
   FormControl,
   FormLabel,
-  Textarea
-} from '@chakra-ui/react'
-import { EditIcon } from '@chakra-ui/icons'
+  Textarea,
+  TableContainer,
+  Table,
+  Thead,
+  Tr,
+  Th,
+  Tbody,
+  Td,
+  Text,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+import { AddIcon, EditIcon, DeleteIcon } from "@chakra-ui/icons";
 
-export default function EditDispute ({
-  title,
-  description
-}: {
-  title: string
-  description: string
-}) {
+interface EditDisputeButtonProps {
+  title: string;
+  description: string;
+  fees: { name: string; cost: string }[];
+}
+
+const EditDisputeButton: React.FC<EditDisputeButtonProps> = ({
+  title: initialTitle,
+  description: initialDescription,
+  fees: initialFees,
+}) => {
+  const [title, setTitle] = useState(initialTitle);
+  const [description, setDescription] = useState(initialDescription);
+  const [fees, setFees] = useState(initialFees);
+
+  const handleFeeChange = (
+    index: number,
+    field: "name" | "cost",
+    value: string
+  ) => {
+    const updatedFees = [...fees];
+    updatedFees[index] = { ...updatedFees[index], [field]: value };
+    setFees(updatedFees);
+  };
+
+  const handleAddFee = () => {
+    setFees([...fees, { name: "", cost: "" }]);
+  };
+
+  const handleRemoveFee = (index: number) => {
+    const updatedFees = [...fees];
+    updatedFees.splice(index, 1);
+    setFees(updatedFees);
+  };
+
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <Button size='sm' mr='5px'>
+        <Button size="sm" mr="5px">
           <EditIcon />
         </Button>
       </DialogTrigger>
-      <DialogContent className='lg:min-w-[800px]'>
-        <form action=''>
+      <DialogContent className="lg:min-w-[800px]">
+        <form action="">
           <DialogHeader>
             <DialogTitle>Update a Dispute </DialogTitle>
             <DialogDescription>
@@ -43,16 +80,16 @@ export default function EditDispute ({
           </DialogHeader>
 
           {/* Form Content */}
-          <Stack spacing='15px' my='2rem'>
+          <Stack spacing="15px" my="2rem">
             {/* Dispute Title */}
             <FormControl isRequired>
-              <FormLabel fontSize='sm' fontWeight='semibold'>
+              <FormLabel fontSize="sm" fontWeight="semibold">
                 Title:
               </FormLabel>
               <Input
-                size='md'
-                fontWeight='semibold'
-                type='string'
+                size="md"
+                fontWeight="semibold"
+                type="string"
                 value={title}
                 disabled
               />
@@ -60,26 +97,85 @@ export default function EditDispute ({
 
             {/* Dispute Description */}
             <FormControl isRequired>
-              <FormLabel fontSize='sm' fontWeight='semibold'>
+              <FormLabel fontSize="sm" fontWeight="semibold">
                 Description:
               </FormLabel>
               <Textarea
-                placeholder='Write something...'
-                fontSize='sm'
-                maxH='300px'
+                placeholder="Write something..."
+                fontSize="sm"
+                fontFamily="font.body"
+                maxH="300px"
                 value={description}
-                resize='none'
+                onChange={(e) => setDescription(e.target.value)}
+                resize="none"
               />
             </FormControl>
+
+            {/* Set Possible Fees and Costs*/}
+            <TableContainer>
+              <Table size="xs" w="80%" fontFamily="font.body">
+                <Thead>
+                  <Tr>
+                    <Th fontSize="xs" fontFamily="font.body">
+                      Fees
+                    </Th>
+                    <Th fontSize="xs" fontFamily="font.body">
+                      Costs
+                    </Th>
+                    <Th>
+                      <Button size="xs" onClick={handleAddFee}>
+                        <AddIcon />
+                      </Button>
+                    </Th>
+                  </Tr>
+                </Thead>
+                <Tbody fontSize="sm" fontFamily="font.body">
+                  {fees.map((fee, index) => (
+                    <Tr key={index}>
+                      <Td>
+                        <Input
+                          size="sm"
+                          value={fee.name}
+                          onChange={(e) =>
+                            handleFeeChange(index, "name", e.target.value)
+                          }
+                        />
+                      </Td>
+                      <Td>
+                        <Input
+                          size="sm"
+                          value={fee.cost}
+                          onChange={(e) =>
+                            handleFeeChange(index, "cost", e.target.value)
+                          }
+                        />
+                      </Td>
+                      <Td>
+                        {fees.length > 1 && index !== 0 && (
+                          <Button
+                            size="xs"
+                            colorScheme="red"
+                            onClick={() => handleRemoveFee(index)}
+                          >
+                            <DeleteIcon />
+                          </Button>
+                        )}
+                      </Td>
+                    </Tr>
+                  ))}
+                </Tbody>
+              </Table>
+            </TableContainer>
           </Stack>
 
           <DialogFooter>
-            <Button size='sm' colorScheme='yellow' type='submit'>
+            <Button size="sm" colorScheme="yellow" type="submit">
               Save Changes
             </Button>
           </DialogFooter>
         </form>
       </DialogContent>
     </Dialog>
-  )
-}
+  );
+};
+export default EditDisputeButton;
