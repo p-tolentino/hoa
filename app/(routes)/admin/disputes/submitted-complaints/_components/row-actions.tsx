bf5e953ac52b59d420350b6e1ec13b150519a39f
kath <kathleen_tan@dlsu.edu.ph>
2024-03-06 02:15:00 +0800

    set follow up button to size sm

--- app/(routes)/admin/disputes/submitted-complaints/_components/row-actions.tsx
+++ app/(routes)/admin/disputes/submitted-complaints/_components/row-actions.tsx
@@ -1,38 +1,42 @@
-import { Button, Wrap, WrapItem, useToast } from '@chakra-ui/react'
-import { useState } from 'react'
+import { Button, Wrap, WrapItem, useToast } from "@chakra-ui/react";
+import { useState } from "react";
 
-import { SubmittedComplaintsColumn } from './columns'
+import { SubmittedComplaintsColumn } from "./columns";
 
 interface RowActionProps {
-  data: SubmittedComplaintsColumn
+  data: SubmittedComplaintsColumn;
 }
 
 export const RowActions: React.FC<RowActionProps> = ({ data }) => {
-  const toast = useToast()
-  const [isButtonClicked, setIsButtonClicked] = useState(false)
+  const toast = useToast();
+  const [isButtonClicked, setIsButtonClicked] = useState(false);
 
   const handleButtonClick = () => {
     if (!isButtonClicked) {
       toast({
         title: `Successfully followed up complaint: ${data.title}`,
         description:
-          'The HOA officers will get back to you as soon as possible.',
-        status: 'success',
-        position: 'bottom-right',
-        isClosable: true
-      })
+          "The HOA officers will get back to you as soon as possible.",
+        status: "success",
+        position: "bottom-right",
+        isClosable: true,
+      });
 
-      setIsButtonClicked(true)
+      setIsButtonClicked(true);
     }
-  }
+  };
 
   return (
     <Wrap>
       <WrapItem>
-        <Button onClick={handleButtonClick} isDisabled={isButtonClicked}>
+        <Button
+          size="sm"
+          onClick={handleButtonClick}
+          isDisabled={isButtonClicked}
+        >
           Follow up
         </Button>
       </WrapItem>
     </Wrap>
-  )
-}
+  );
+};
