5d152a696cf411ff5e982f706f43887d1ab8396d
kath <kathleen_tan@dlsu.edu.ph>
2024-03-07 01:00:41 +0800

    fixed Events Page width sizing

--- app/(routes)/admin/community/page.tsx
+++ app/(routes)/admin/community/page.tsx
@@ -1,23 +1,26 @@
-import { Heading } from '@/components/ui/heading'
-import { Separator } from '@/components/ui/separator'
-import { Flex } from '@chakra-ui/react'
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { Flex } from "@chakra-ui/react";
 
 import { currentUser } from "@/lib/auth";
 
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
-import DiscussionsCard from './_components/discussions/DiscussionsCard'
-import AnnouncementBoard from './_components/sideCards/announcements/AnnouncementBoard'
-import HomeownerResources from './_components/sideCards/homeownerResources/HomeownerResources'
-import AdminOfficerLinks from './_components/sideCards/adminOfficerLinks/AdminOfficerLinks'
-import BusinessForumCard from './_components/business/BusinessForumCard'
-import PollsAndSurveysCard from './_components/pollsAndSurveys/PollsAndSurveysCard'
-import EventsCard from './_components/events/EventsCard'
-import { getPosts, getDiscussionPosts, getBusinessPosts } from "@/server/data/posts";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import DiscussionsCard from "./_components/discussions/DiscussionsCard";
+import AnnouncementBoard from "./_components/sideCards/announcements/AnnouncementBoard";
+import HomeownerResources from "./_components/sideCards/homeownerResources/HomeownerResources";
+import AdminOfficerLinks from "./_components/sideCards/adminOfficerLinks/AdminOfficerLinks";
+import BusinessForumCard from "./_components/business/BusinessForumCard";
+import PollsAndSurveysCard from "./_components/pollsAndSurveys/PollsAndSurveysCard";
+import EventsCard from "./_components/events/EventsCard";
+import {
+  getPosts,
+  getDiscussionPosts,
+  getBusinessPosts,
+} from "@/server/data/posts";
 import { getPolls } from "@/server/data/polls";
 import { getEvents } from "@/server/data/events";
 
 const Community = async () => {
-
   const discussion = await getDiscussionPosts();
   if (!discussion) {
     return null;
@@ -28,10 +31,9 @@ const Community = async () => {
     return null;
   }
 
-  const filteredPosts1 =discussion.filter(post => post.status==="ACTIVE")
-  const filteredPosts2 =business.filter(post => post.status==="ACTIVE")
+  const filteredPosts1 = discussion.filter((post) => post.status === "ACTIVE");
+  const filteredPosts2 = business.filter((post) => post.status === "ACTIVE");
 
-  
   const polls = await getPolls();
   if (!polls) {
     return null;
@@ -41,7 +43,7 @@ const Community = async () => {
   if (!events) {
     return null;
   }
-  
+
   const user = await currentUser();
 
   if (!user) {
@@ -51,39 +53,39 @@ const Community = async () => {
   return (
     <>
       <Heading
-        title='Community Engagement'
-        description='Navigate through the Community Engagement module'
+        title="Community Engagement"
+        description="Navigate through the Community Engagement module"
       />
-      <Separator className='mt-4 mb-6' />
-      <Flex gap='2rem' w='78vw'>
-        <Tabs defaultValue='discussions' className='w-[70vw]'>
-          <TabsList className='grid w-full grid-cols-4'>
-            <TabsTrigger value='discussions'>Discussions</TabsTrigger>
-            <TabsTrigger value='business'>Business Forum</TabsTrigger>
-            <TabsTrigger value='pollsAndSurveys'>Polls & Surveys</TabsTrigger>
-            <TabsTrigger value='events'>Events</TabsTrigger>
+      <Separator className="mt-4 mb-6" />
+      <Flex gap="2rem" w="85vw">
+        <Tabs defaultValue="discussions" className="w-[70vw]">
+          <TabsList className="grid w-full grid-cols-4">
+            <TabsTrigger value="discussions">Discussions</TabsTrigger>
+            <TabsTrigger value="business">Business Forum</TabsTrigger>
+            <TabsTrigger value="pollsAndSurveys">Polls & Surveys</TabsTrigger>
+            <TabsTrigger value="events">Events</TabsTrigger>
           </TabsList>
-          <TabsContent value='discussions'>
-            <DiscussionsCard posts={filteredPosts1} user={user.id}/>
+          <TabsContent value="discussions">
+            <DiscussionsCard posts={filteredPosts1} user={user.id} />
           </TabsContent>
-          <TabsContent value='business'>
-            <BusinessForumCard posts={filteredPosts2} user={user.id}/>
+          <TabsContent value="business">
+            <BusinessForumCard posts={filteredPosts2} user={user.id} />
           </TabsContent>
-          <TabsContent value='pollsAndSurveys'>
-            <PollsAndSurveysCard polls={polls} user={user.id}/>
+          <TabsContent value="pollsAndSurveys">
+            <PollsAndSurveysCard polls={polls} user={user.id} />
           </TabsContent>
-          <TabsContent value='events'>
-            <EventsCard events={events} user={user.id}/>
+          <TabsContent value="events">
+            <EventsCard events={events} user={user.id} />
           </TabsContent>
         </Tabs>
-        <Flex flexDir='column' gap='2rem'>
+        <Flex flexDir="column" gap="2rem">
           <AnnouncementBoard />
           <HomeownerResources />
           <AdminOfficerLinks />
         </Flex>
       </Flex>
     </>
-  )
-}
+  );
+};
 
-export default Community
+export default Community;
