491e544e3fc1197bb1dcab9e738cf64a7b2e1e4a
kath <kathleen_tan@dlsu.edu.ph>
2024-03-08 01:33:55 +0800

    updated dispute-record (changed View Complaint to View Dispute Form)

--- app/(routes)/admin/disputes/dispute-record/_components/columns.tsx
+++ app/(routes)/admin/disputes/dispute-record/_components/columns.tsx
@@ -1,71 +1,71 @@
-'use client'
+"use client";
 
-import { ColumnDef } from '@tanstack/react-table'
-import { RowActions } from './row-actions'
-import { Button } from '@/components/ui/button'
-import { ArrowUpDown } from 'lucide-react'
-import { Badge } from '@/components/ui/badge'
-import { cn } from '@/lib/utils'
+import { ColumnDef } from "@tanstack/react-table";
+import { RowActions } from "./row-actions";
+import { Button } from "@/components/ui/button";
+import { ArrowUpDown } from "lucide-react";
+import { Badge } from "@/components/ui/badge";
+import { cn } from "@/lib/utils";
 
 export type ListOfDisputesColumn = {
-  id: string
-  status: string
-  dateSubmitted: string
-  submittedBy: string
-  viewDisputeForm: string
-}
+  id: string;
+  status: string;
+  dateSubmitted: string;
+  submittedBy: string;
+  viewDisputeForm: string;
+};
 
 export const columns: ColumnDef<ListOfDisputesColumn>[] = [
   {
-    accessorKey: 'status',
+    accessorKey: "status",
     header: ({ column }) => {
       return (
         <Button
-          variant='ghost'
-          className='hover:bg-[#ffe492]'
-          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+          variant="ghost"
+          className="hover:bg-[#ffe492]"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
         >
           Status
-          <ArrowUpDown className='w-4 h-4 ml-2' />
+          <ArrowUpDown className="w-4 h-4 ml-2" />
         </Button>
-      )
+      );
     },
     cell: ({ row }) => (
       <Badge
         className={cn(
-          row.getValue('status') === 'Resolved'
-            ? 'bg-green-700'
-            : row.getValue('status') === 'Pending'
-            ? 'bg-red-700'
-            : row.getValue('status') === 'In Process'
-            ? 'bg-yellow-600'
-            : 'display-none'
+          row.getValue("status") === "Resolved"
+            ? "bg-green-700"
+            : row.getValue("status") === "Pending"
+            ? "bg-red-700"
+            : row.getValue("status") === "In Process"
+            ? "bg-yellow-600"
+            : "display-none"
         )}
       >
-        {' '}
-        {row.getValue('status')}
+        {" "}
+        {row.getValue("status")}
       </Badge>
-    )
+    ),
   },
   {
-    accessorKey: 'dateSubmitted',
-    header: 'Date Submitted'
+    accessorKey: "dateSubmitted",
+    header: "Date Submitted",
   },
   {
-    accessorKey: 'submittedBy',
-    header: 'Submitted By'
+    accessorKey: "submittedBy",
+    header: "Submitted By",
   },
   {
-    accessorKey: 'viewDisputeForm',
-    header: 'View Complaint',
+    accessorKey: "viewDisputeForm",
+    header: "View Dispute Form",
     cell: ({ row }) => (
-      <a href={'/admin/disputes/view-progress'} className='hover:underline'>
+      <a href={"/admin/disputes/view-progress"} className="hover:underline">
         {row.original.viewDisputeForm}
       </a>
-    )
+    ),
   },
   {
-    id: 'actions',
-    cell: ({ row }) => <RowActions data={row.original} />
-  }
-]
+    id: "actions",
+    cell: ({ row }) => <RowActions data={row.original} />,
+  },
+];
