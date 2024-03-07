a3caadfdd3cdc95e4f573cdf0f21bb32a8582f22
maxinefeliciano <a.maxine.feliciano@gmail.com>
2024-03-08 01:59:46 +0800

    Fixed errors in sample dispute letter/notice

--- app/(routes)/admin/disputes/dispute-record/page.tsx
+++ app/(routes)/admin/disputes/dispute-record/page.tsx
@@ -1,36 +1,36 @@
-import React from "react";
-import { ListOfDisputesClient } from "./_components/client";
+import React from 'react'
+import { ListOfDisputesClient } from './_components/client'
 
-export default function Disputes() {
+export default function Disputes () {
   const data = [
     {
-      id: "12345",
-      status: "Pending",
-      dateSubmitted: "02/14/24",
-      submittedBy: "Dela Cruz, Juan (Homeowner)",
-      viewDisputeForm: "Sample Link to Dispute 1",
+      id: '12345',
+      status: 'Pending',
+      dateSubmitted: '02/14/24',
+      submittedBy: 'Dela Cruz, Juan (Homeowner)',
+      viewDisputeForm: 'Sample Link to Dispute Form 1'
     },
     {
-      id: "67890",
-      status: "In Process",
-      dateSubmitted: "02/20/24",
-      submittedBy: "Ibarra, Crisostomo (Homeowner)",
-      viewDisputeForm: "Sample Link to Dispute 2",
+      id: '67890',
+      status: 'In Process',
+      dateSubmitted: '02/20/24',
+      submittedBy: 'Ibarra, Crisostomo (Homeowner)',
+      viewDisputeForm: 'Sample Link to Dispute Form 2'
     },
     {
-      id: "54321",
-      status: "Resolved",
-      dateSubmitted: "02/27/24",
-      submittedBy: "Dela Cruz, Juanita (Homeowner)",
-      viewDisputeForm: "Sample Link to Dispute 3",
-    },
-  ];
+      id: '54321',
+      status: 'Resolved',
+      dateSubmitted: '02/27/24',
+      submittedBy: 'Dela Cruz, Juanita (Homeowner)',
+      viewDisputeForm: 'Sample Link to Dispute Form 3'
+    }
+  ]
 
   return (
     <div>
-      <div className="flex-1 space-y-4">
+      <div className='flex-1 space-y-4'>
         <ListOfDisputesClient data={data} />
       </div>
     </div>
-  );
+  )
 }
--- app/(routes)/admin/disputes/letters-and-notices/sample/page.tsx
+++ app/(routes)/admin/disputes/letters-and-notices/sample/page.tsx
@@ -35,85 +35,83 @@ export default function MeetingNotice () {
         </Button>
       </Box>
       <Center flexDir='column'>
-        <Text
-          my='10px'
-          fontSize='xl'
-          fontWeight='bold'
-          fontFamily='font.heading'
-        >
-          Dispute Resolution Meeting Notice 📅
-        </Text>
-        <Box borderWidth='1px' p={10} borderRadius='md' w='50%'>
-          <Stack spacing={5} fontFamily='font.body' fontSize='lg'>
-            <Flex justifyContent='space-between'>
-              {/* Recipient */}
-              <Text>
-                Dear <span className='font-bold'>{recipient}</span>,
+        <>
+          <Text
+            my='10px'
+            fontSize='xl'
+            fontWeight='bold'
+            fontFamily='font.heading'
+          >
+            Dispute Resolution Meeting Notice 📅
+          </Text>
+          <Box borderWidth='1px' p={10} borderRadius='md' w='50%'>
+            <Stack spacing={5} fontFamily='font.body' fontSize='lg'>
+              <Flex justifyContent='space-between'>
+                {/* Recipient */}
+                <Text>
+                  Dear <span className='font-bold'>{recipient}</span>,
+                </Text>
+                {/* Date Received */}
+                <Text fontWeight='bold'>{dateReceived}</Text>
+              </Flex>
+              {/* Dispute Type */}
+              <Text textAlign='justify'>
+                You are hereby summoned to attend a meeting regarding the
+                ongoing dispute: <br />
+                <span className='font-bold text-xl'>{disputeType}</span>
               </Text>
 
-              {/* Date Received */}
-              <Text fontWeight='bold'>{dateReceived}</Text>
-            </Flex>
+              {/* Involved Parties (in 2 columns) */}
+              <Box>
+                <Text>Involved parties:</Text>
+                <Flex>
+                  <UnorderedList>
+                    {involvedParties
+                      .slice(0, Math.ceil(involvedParties.length / 2))
+                      .map((person, index) => (
+                        <ListItem key={index}>{person}</ListItem>
+                      ))}
+                  </UnorderedList>
+                  <UnorderedList ml={10}>
+                    {involvedParties
+                      .slice(Math.ceil(involvedParties.length / 2))
+                      .map((person, index) => (
+                        <ListItem key={index}>{person}</ListItem>
+                      ))}
+                  </UnorderedList>
+                </Flex>
+              </Box>
 
-            {/* Dispute Type */}
-            <Text textAlign='justify'>
-              You are hereby summoned to attend a meeting regarding the ongoing
-              dispute: <br />
-              <span className='font-bold text-xl'>{disputeType}</span>
-            </Text>
-
-            {/* Involved Parties (in 2 columns) */}
-            <Text>
-              Involved parties: <br />
-              <Flex>
-                <UnorderedList>
-                  {involvedParties
-                    .slice(0, Math.ceil(involvedParties.length / 2))
-                    .map((person, index) => (
-                      <ListItem key={index}>{person}</ListItem>
-                    ))}
-                </UnorderedList>
-                <UnorderedList ml={10}>
-                  {involvedParties
-                    .slice(Math.ceil(involvedParties.length / 2))
-                    .map((person, index) => (
-                      <ListItem key={index}>{person}</ListItem>
-                    ))}
-                </UnorderedList>
-              </Flex>
-            </Text>
-
-            {/* Meeting Data, Time and Venue */}
-            <Text textAlign='justify'>
-              The meeting is scheduled for{' '}
-              <span className='relative rounded bg-yellow-100 px-2 py-1 font-semibold'>
-                {dateAndTime}
-              </span>{' '}
-              at the{' '}
-              <span className='relative rounded bg-yellow-100 px-2 py-1 font-semibold'>
-                {meetingVenue}
-                {'. '}
-              </span>
-              Your presence is crucial for resolving this matter.
-            </Text>
-
-            <Text textAlign='justify'>
-              Details of the dispute and proposed resolutions will be discussed.
-              Please come prepared to express your perspective and work towards
-              a resolution.
-            </Text>
-            <Text>Thank you for your cooperation.</Text>
-            <Text textAlign='justify' mt={5}>
-              Sincerely,
-            </Text>
-
-            {/* Sender's Name and Position */}
-            <Box>
-              <Text>{sender.name}</Text>
-              <Text color='grey'>{sender.position}</Text>
-            </Box>
-          </Stack>
-        </Box>
+              {/* Meeting Data, Time and Venue */}
+              <Text textAlign='justify'>
+                The meeting is scheduled for{' '}
+                <span className='relative rounded bg-yellow-100 px-2 py-1 font-semibold'>
+                  {dateAndTime}
+                </span>{' '}
+                at the{' '}
+                <span className='relative rounded bg-yellow-100 px-2 py-1 font-semibold'>
+                  {meetingVenue}
+                  {'. '}
+                </span>
+                Your presence is crucial for resolving this matter.
+              </Text>
+              <Text textAlign='justify'>
+                Details of the dispute and proposed resolutions will be
+                discussed. Please come prepared to express your perspective and
+                work towards a resolution.
+              </Text>
+              <Text>Thank you for your cooperation.</Text>
+              <Text textAlign='justify' mt={5}>
+                Sincerely,
+              </Text>
+              {/* Sender's Name and Position */}
+              <Box>
+                <Text>{sender.name}</Text>
+                <Text color='grey'>{sender.position}</Text>
+              </Box>
+            </Stack>
+          </Box>
+        </>
       </Center>
     </div>
   )
