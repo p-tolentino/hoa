2d0e77208f2e624a8de4f6a8c3606d2692354fc4
maxinefeliciano <a.maxine.feliciano@gmail.com>
2024-03-08 01:48:51 +0800

    Dispute Letters and Notices data table added

--- app/(routes)/admin/disputes/letters-and-notices/page.tsx
+++ app/(routes)/admin/disputes/letters-and-notices/page.tsx
@@ -1,16 +1,37 @@
-import { Button, Text } from '@chakra-ui/react'
-import Link from 'next/link'
 import React from 'react'
+import { DisputeLettersAndNoticesClient } from './_components/client'
 
 export default function DisputeLettersAndNotices () {
+  const data = [
+    {
+      id: '12345',
+      dateReceived: '02/20/24',
+      disputeType: 'Neighbor-to-Neighbor Conflict',
+      sender: 'HOA President',
+      viewDisputeLetterNotice: 'Sample Link to Letter/Notice 1'
+    },
+    {
+      id: '67890',
+      dateReceived: '02/20/24',
+      disputeType: 'Neighbor-to-Neighbor Conflict',
+      sender: 'HOA President',
+
+      viewDisputeLetterNotice: 'Sample Link to Letter/Notice 2'
+    },
+    {
+      id: '54321',
+      dateReceived: '02/20/24',
+      disputeType: 'Neighbor-to-Neighbor Conflict',
+      sender: 'HOA President',
+      viewDisputeLetterNotice: 'Sample Link to Letter/Notice 3'
+    }
+  ]
+
   return (
     <div>
-      <Button as={Link} href='/admin/disputes/letters-and-notices/sample'>
-        Go to Sample Notice
-      </Button>
-      <Text mt={5}>
-        Data table for all received dispute letters and notices here
-      </Text>
+      <div className='flex-1 space-y-4'>
+        <DisputeLettersAndNoticesClient data={data} />
+      </div>
     </div>
   )
 }
