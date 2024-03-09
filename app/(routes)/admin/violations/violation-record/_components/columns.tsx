661df7145a0787bd7f24923f5c6e827bf3752405
maxinefeliciano <a.maxine.feliciano@gmail.com>
2024-03-09 16:13:09 +0800

    Violation Enforcement Progress updated
    Changed viewViolationForm to enforcementProgress

--- app/(routes)/admin/violations/violation-record/_components/columns.tsx
+++ app/(routes)/admin/violations/violation-record/_components/columns.tsx
@@ -1,74 +1,76 @@
-"use client";
+'use client'
 
-import { ColumnDef } from "@tanstack/react-table";
-import { RowActions } from "./row-actions";
-import { Button } from "@/components/ui/button";
-import { ArrowUpDown } from "lucide-react";
-import { Badge } from "@/components/ui/badge";
-import { cn } from "@/lib/utils";
+import { ColumnDef } from '@tanstack/react-table'
+import { RowActions } from './row-actions'
+import { Button } from '@/components/ui/button'
+import { ArrowUpDown } from 'lucide-react'
+import { Badge } from '@/components/ui/badge'
+import { cn } from '@/lib/utils'
 
 export type ListOfViolationsColumn = {
-  id: string;
-  status: string;
-  dateSubmitted: string;
-  submittedBy: string;
-  viewViolationForm: string;
-};
+  id: string
+  status: string
+  dateSubmitted: string
+  submittedBy: string
+  enforcementProgress: string
+}
 
 export const columns: ColumnDef<ListOfViolationsColumn>[] = [
   {
-    accessorKey: "status",
+    accessorKey: 'status',
     header: ({ column }) => {
       return (
         <Button
-          variant="ghost"
-          className="hover:bg-[#ffe492]"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          variant='ghost'
+          className='hover:bg-[#ffe492]'
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
         >
           Status
-          <ArrowUpDown className="w-4 h-4 ml-2" />
+          <ArrowUpDown className='w-4 h-4 ml-2' />
         </Button>
-      );
+      )
     },
     cell: ({ row }) => (
       <Badge
         className={cn(
-          row.getValue("status") === "Resolved"
-            ? "bg-green-700"
-            : row.getValue("status") === "Pending"
-            ? "bg-red-700"
-            : row.getValue("status") === "In Process"
-            ? "bg-yellow-600"
-            : "display-none"
+          row.getValue('status') === 'Resolved'
+            ? 'bg-green-700'
+            : row.getValue('status') === 'Pending'
+            ? 'bg-red-700'
+            : row.getValue('status') === 'In Process'
+            ? 'bg-yellow-600'
+            : 'display-none'
         )}
       >
-        {" "}
-        {row.getValue("status")}
+        {' '}
+        {row.getValue('status')}
       </Badge>
-    ),
+    )
   },
   {
-    accessorKey: "dateSubmitted",
-    header: "Date Submitted",
+    accessorKey: 'dateSubmitted',
+    header: 'Date Submitted'
   },
   {
-    accessorKey: "submittedBy",
-    header: "Submitted By",
+    accessorKey: 'submittedBy',
+    header: 'Submitted By'
   },
   {
-    accessorKey: "viewViolationForm",
-    header: "View Violation Form",
+    accessorKey: 'enforcementProgress',
+    header: 'Enforcement Progress',
     cell: ({ row }) => (
       <a
-        href={"/admin/violations/violation-record/view-progress"}
-        className="hover:underline"
+        href={'/admin/violations/violation-record/view-progress'}
+        className='hover:underline hover:text-blue-500'
       >
-        {row.original.viewViolationForm}
+        {row.original.enforcementProgress}
       </a>
-    ),
+    )
   },
   {
-    id: "actions",
-    cell: ({ row }) => <RowActions data={row.original} />,
-  },
-];
+    accessorKey: 'actions',
+    header: 'Actions',
+    id: 'actions',
+    cell: ({ row }) => <RowActions data={row.original} />
+  }
+]
--- app/(routes)/admin/violations/violation-record/page.tsx
+++ app/(routes)/admin/violations/violation-record/page.tsx
@@ -1,36 +1,37 @@
-import React from "react";
-import { ListOfViolationsClient } from "./_components/client";
+import React from 'react'
+import { ListOfViolationsClient } from './_components/client'
 
-export default function Violations() {
+export default function Violations () {
   const data = [
     {
-      id: "12345",
-      status: "Pending",
-      dateSubmitted: "02/14/24",
-      submittedBy: "Dela Cruz, Juan (Homeowner)",
-      viewViolationForm: "Sample Link to Violation Form 1",
+      id: '12345',
+      status: 'Pending',
+      dateSubmitted: '02/14/24',
+      submittedBy: 'Dela Cruz, Juan (Homeowner)',
+      enforcementProgress: 'Step 1: Violation Form Submission'
     },
     {
-      id: "67890",
-      status: "In Process",
-      dateSubmitted: "02/20/24",
-      submittedBy: "Ibarra, Crisostomo (Homeowner)",
-      viewViolationForm: "Sample Link to Violation Form 2",
+      id: '67890',
+      status: 'In Process',
+      dateSubmitted: '02/20/24',
+      submittedBy: 'Ibarra, Crisostomo (Homeowner)',
+      enforcementProgress:
+        'Step 4: Review by Environment and Security Committee'
     },
     {
-      id: "54321",
-      status: "Resolved",
-      dateSubmitted: "02/27/24",
-      submittedBy: "Dela Cruz, Juanita (Homeowner)",
-      viewViolationForm: "Sample Link to Violation Form 3",
-    },
-  ];
+      id: '54321',
+      status: 'Resolved',
+      dateSubmitted: '02/27/24',
+      submittedBy: 'Dela Cruz, Juanita (Homeowner)',
+      enforcementProgress: ''
+    }
+  ]
 
   return (
     <div>
-      <div className="flex-1 space-y-4">
+      <div className='flex-1 space-y-4'>
         <ListOfViolationsClient data={data} />
       </div>
     </div>
-  );
+  )
 }
