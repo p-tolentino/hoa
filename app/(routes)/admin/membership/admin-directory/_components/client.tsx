9313b837db22b78a768b9a15c9c6208e557a0ea4
kath <kathleen_tan@dlsu.edu.ph>
2024-03-13 15:41:59 +0800

    pdftable global component

--- app/(routes)/admin/membership/admin-directory/_components/client.tsx
+++ app/(routes)/admin/membership/admin-directory/_components/client.tsx
@@ -3,13 +3,14 @@
 import React from "react";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
-import { Button } from "@chakra-ui/react";
+import { Button, Table, Th, Tr, Td, Thead, Tbody } from "@chakra-ui/react";
 
 import { AdminColumn, columns } from "./columns";
 import { DataTable } from "@/components/ui/data-table";
 
 import { useReactToPrint } from "react-to-print";
 import { useRef } from "react";
+import PDFTable from "@/components/system/PDFTable";
 
 interface AdminsClientProps {
   data: AdminColumn[];
@@ -36,9 +37,12 @@ export const AdminsClient: React.FC<AdminsClientProps> = ({ data }) => {
         </Button>
       </div>
       <Separator />
-      <div ref={componentPDF} style={{ width: "100%" }}>
-        <DataTable columns={columns} data={data} searchKey="email" />
+      <div className="hidden">
+        <div ref={componentPDF} style={{ width: "100%" }}>
+          <PDFTable />
+        </div>
       </div>
+      <DataTable columns={columns} data={data} searchKey="email" />
     </>
   );
 };
