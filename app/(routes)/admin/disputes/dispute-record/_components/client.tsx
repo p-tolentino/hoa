306f942f752a51b5ea97913dbfb3e3d1bb04e42a
kath <kathleen_tan@dlsu.edu.ph>
2024-03-13 18:37:05 +0800

    DISPUTE initialized report generation

--- app/(routes)/admin/disputes/dispute-record/_components/client.tsx
+++ app/(routes)/admin/disputes/dispute-record/_components/client.tsx
@@ -1,34 +1,58 @@
-'use client'
+"use client";
 
-import { Heading } from '@/components/ui/heading'
-import { Separator } from '@/components/ui/separator'
+import React from "react";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
 
-import { ListOfDisputesColumn, columns } from './columns'
-import { DataTable } from '@/components/ui/data-table'
+import { ListOfDisputesColumn, columns } from "./columns";
+import { DataTable } from "@/components/ui/data-table";
 
-import { Flex, Button } from '@chakra-ui/react'
-import Link from 'next/link'
+import { Flex, Button, HStack } from "@chakra-ui/react";
+import Link from "next/link";
+
+import { useReactToPrint } from "react-to-print";
+import { useRef } from "react";
+import PDFTable from "@/components/system/PDFTable";
 
 interface ListOfDisputesClientProps {
-  data: ListOfDisputesColumn[]
+  data: ListOfDisputesColumn[];
 }
 
 export const ListOfDisputesClient: React.FC<ListOfDisputesClientProps> = ({
-  data
+  data,
 }) => {
+  const componentPDF = useRef<HTMLDivElement | null>(null);
+
+  const generatePDF = useReactToPrint({
+    content: () => componentPDF.current || null,
+    documentTitle: "Homeowners' Association Dispute Report",
+    onAfterPrint: () => alert("Data saved in PDF"),
+  });
   return (
     <>
-      <Flex justify='space-between'>
+      <Flex justify="space-between">
         <Heading
           title="Homeowners' Association Dispute Record"
-          description='Manage the submitted dispute forms of Homeowners.'
+          description="Manage the submitted dispute forms of Homeowners."
         />
-        <Button size='sm' as={Link} href='/admin/disputes'>
-          Go Back
-        </Button>
+        <HStack>
+          <Button size="sm" colorScheme="yellow" onClick={generatePDF}>
+            Generate PDF
+          </Button>
+          <Button size="sm" as={Link} href="/admin/disputes">
+            Go Back
+          </Button>
+        </HStack>
       </Flex>
       <Separator />
-      <DataTable columns={columns} data={data} searchKey='createdAt' />
+      {/* <div className="hidden">
+        <div ref={componentPDF} style={{ width: "100%" }}>
+          <PDFTable />
+        </div>
+      </div> */}
+      <div ref={componentPDF} style={{ width: "100%" }}>
+        <DataTable columns={columns} data={data} searchKey="createdAt" />
+      </div>
     </>
-  )
-}
+  );
+};
