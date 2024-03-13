8cba96d5ae3040d4f9b9eee99c99eed33dc7457a
kath <kathleen_tan@dlsu.edu.ph>
2024-03-13 18:37:33 +0800

    VIOLATIONS initialized report generation

--- app/(routes)/admin/violations/submitted-violations/_components/client.tsx
+++ app/(routes)/admin/violations/submitted-violations/_components/client.tsx
@@ -1,13 +1,18 @@
 "use client";
 
+import React from "react";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
 
 import { SubmittedViolationsColumn, columns } from "./columns";
 import { DataTable } from "@/components/ui/data-table";
 
-import { Flex, Button } from "@chakra-ui/react";
+import { Flex, Button, HStack } from "@chakra-ui/react";
 import Link from "next/link";
+
+import { useReactToPrint } from "react-to-print";
+import { useRef } from "react";
+import PDFTable from "@/components/system/PDFTable";
 
 interface SubmittedViolationsClientProps {
   data: SubmittedViolationsColumn[];
@@ -16,6 +21,13 @@ interface SubmittedViolationsClientProps {
 export const SubmittedViolationsClient: React.FC<
   SubmittedViolationsClientProps
 > = ({ data }) => {
+  const componentPDF = useRef<HTMLDivElement | null>(null);
+
+  const generatePDF = useReactToPrint({
+    content: () => componentPDF.current || null,
+    documentTitle: "Submitted Violation Report",
+    onAfterPrint: () => alert("Data saved in PDF"),
+  });
   return (
     <>
       <Flex justify="space-between">
@@ -23,12 +35,24 @@ export const SubmittedViolationsClient: React.FC<
           title="Submitted Violation Reports"
           description="Monitor the progress of your submitted violation reports to the Homeowners' Association."
         />
-        <Button size="sm" colorScheme="gray" as={Link} href="/admin/violations">
-          Go Back
-        </Button>
+        <HStack>
+          <Button size="sm" colorScheme="yellow" onClick={generatePDF}>
+            Generate PDF
+          </Button>
+          <Button size="sm" as={Link} href="/admin/violations">
+            Go Back
+          </Button>
+        </HStack>
       </Flex>
       <Separator />
-      <DataTable columns={columns} data={data} searchKey="createdAt" />
+      {/* <div className="hidden">
+        <div ref={componentPDF} style={{ width: "100%" }}>
+          <PDFTable />
+        </div>
+      </div> */}
+      <div ref={componentPDF} style={{ width: "100%" }}>
+        <DataTable columns={columns} data={data} searchKey="createdAt" />
+      </div>
     </>
   );
 };
