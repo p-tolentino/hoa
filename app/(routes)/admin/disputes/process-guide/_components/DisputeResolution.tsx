775c2ece463d3f17ff43ca09766201afea12227b
p-tolentino <95534051+p-tolentino@users.noreply.github.com>
2024-03-15 16:38:06 +0800

    fixed bylaws for dispute page

--- app/(routes)/admin/disputes/process-guide/_components/DisputeResolution.tsx
+++ app/(routes)/admin/disputes/process-guide/_components/DisputeResolution.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import {
   Step,
   StepDescription,
@@ -18,57 +18,57 @@ import {
   UnorderedList,
   ListItem,
   CardHeader,
-  Link
-} from '@chakra-ui/react'
-import DisputeBylaws from './DisputeBylaws'
-import React from 'react'
+  Link,
+} from "@chakra-ui/react";
+import DisputeBylaws from "./DisputeBylaws";
+import { Hoa } from "@prisma/client";
 
-export default function DisputeResolution () {
+export default function DisputeResolution({ hoa }: { hoa: Hoa }) {
   const processSteps = [
     {
-      title: 'Dispute Form Submission',
+      title: "Dispute Form Submission",
       description:
-        'Homeowners submit dispute reports through the Dispute Resolution module in the MIS.',
+        "Homeowners submit dispute reports through the Dispute Resolution module in the MIS.",
       details: [
-        'Homeowners provide details about the dispute, including the type of dispute, type of violation (if applicable), date, and a detailed description of the dispute.',
-        'Supporting evidence such as photos or documents may be attached to the dispute report.'
-      ]
+        "Homeowners provide details about the dispute, including the type of dispute, type of violation (if applicable), date, and a detailed description of the dispute.",
+        "Supporting evidence such as photos or documents may be attached to the dispute report.",
+      ],
     },
     {
-      title: 'Review by Grievance and Adjudication Committee',
+      title: "Review by Grievance and Adjudication Committee",
       description:
-        'The Grievance and Adjudication Committee receives and reviews the dispute report.',
+        "The Grievance and Adjudication Committee receives and reviews the dispute report.",
       details: [
-        'The Grievance and Adjudication Committee receives the dispute report in the MIS and assigns an officer to oversee its resolution.',
-        'Each person involved in the dispute receives a letter outlining the nature of the dispute and a scheduled meeting to deliberate on the most effective course of action for its resolution.',
-        'Prior to the meeting date, the disputing parties are responsible to collect additional documents in support of their respective narratives.'
-      ]
+        "The Grievance and Adjudication Committee receives the dispute report in the MIS and assigns an officer to oversee its resolution.",
+        "Each person involved in the dispute receives a letter outlining the nature of the dispute and a scheduled meeting to deliberate on the most effective course of action for its resolution.",
+        "Prior to the meeting date, the disputing parties are responsible to collect additional documents in support of their respective narratives.",
+      ],
     },
     {
-      title: 'Dispute Resolution with Corrective Actions',
+      title: "Dispute Resolution with Corrective Actions",
       description:
-        'The Officer assigned takes appropriate actions based on their decision, including issuing a penalty fee to violators if applicable.',
+        "The Officer assigned takes appropriate actions based on their decision, including issuing a penalty fee to violators if applicable.",
       details: [
-        'At this step, it is reasonable to presume that the scheduled dispute resolution meeting has been conducted.',
-        'If applicable, each person involved in the dispute is informed that the penalty fee is added to their statement of account, which can be accessed via the Finance Management module.'
-      ]
-    }
-  ]
+        "At this step, it is reasonable to presume that the scheduled dispute resolution meeting has been conducted.",
+        "If applicable, each person involved in the dispute is informed that the penalty fee is added to their statement of account, which can be accessed via the Finance Management module.",
+      ],
+    },
+  ];
 
   const { activeStep, setActiveStep } = useSteps({
     index: 0,
-    count: processSteps.length
-  })
+    count: processSteps.length,
+  });
 
   return (
     <div>
-      <Box mb='1rem'>
+      <Box mb="1rem">
         {/* Section Title */}
-        <Text fontSize='xl' fontFamily='font.heading' fontWeight='bold'>
+        <Text fontSize="xl" fontFamily="font.heading" fontWeight="bold">
           Dispute Resolution
         </Text>
         {/* Section Description */}
-        <Text fontSize='sm' fontFamily='font.body' color='grey'>
+        <Text fontSize="sm" fontFamily="font.body" color="grey">
           You may click any step in the stepper to view its detailed
           description.
         </Text>
@@ -76,12 +76,12 @@ export default function DisputeResolution () {
       <Flex gap={10}>
         <Stepper
           index={activeStep}
-          orientation='vertical'
-          width='min-content'
-          gap='0'
-          colorScheme='yellow'
-          size='md'
-          h='40vh'
+          orientation="vertical"
+          width="min-content"
+          gap="0"
+          colorScheme="yellow"
+          size="md"
+          h="40vh"
         >
           {processSteps.map((step, index) => (
             <Step key={index} onClick={() => setActiveStep(index)}>
@@ -93,9 +93,9 @@ export default function DisputeResolution () {
                 />
               </StepIndicator>
               <Box
-                flexShrink='0'
-                fontFamily='font.body'
-                w='10vw'
+                flexShrink="0"
+                fontFamily="font.body"
+                w="10vw"
                 onClick={() => Request}
               >
                 {/* Stepper Number and Title */}
@@ -106,37 +106,37 @@ export default function DisputeResolution () {
             </Step>
           ))}
         </Stepper>
-        <Box w='100%'>
+        <Box w="100%">
           <Card
-            shadow='lg'
-            mb='1rem'
-            h='48vh'
-            p='20px 20px 30px 20px'
-            overflowY='auto'
+            shadow="lg"
+            mb="1rem"
+            h="48vh"
+            p="20px 20px 30px 20px"
+            overflowY="auto"
           >
             <CardHeader pb={0}>
               <Text
-                fontSize='sm'
-                fontFamily='font.body'
-                color='brand.500'
-                fontWeight='bold'
+                fontSize="sm"
+                fontFamily="font.body"
+                color="brand.500"
+                fontWeight="bold"
               >
                 Step {activeStep + 1}
               </Text>
-              <Text fontSize='lg' fontFamily='font.heading' fontWeight='bold'>
+              <Text fontSize="lg" fontFamily="font.heading" fontWeight="bold">
                 {/* Step Title */}
                 {processSteps[activeStep].title}
               </Text>
-              <Text fontFamily='font.body' textAlign='justify'>
+              <Text fontFamily="font.body" textAlign="justify">
                 {/* Step Description */}
                 {processSteps[activeStep].description}
               </Text>
             </CardHeader>
             <Card />
             <CardBody>
-              <Box fontFamily='font.body' fontSize='sm' textAlign='justify'>
+              <Box fontFamily="font.body" fontSize="sm" textAlign="justify">
                 <Text>Details:</Text>
-                <UnorderedList mb='1rem' ml={7}>
+                <UnorderedList mb="1rem" ml={7}>
                   {processSteps[activeStep].details.map((detail, index) => (
                     <ListItem key={index}>{detail}</ListItem>
                   ))}
@@ -144,17 +144,17 @@ export default function DisputeResolution () {
 
                 {/* More Information */}
                 {activeStep === 0 && (
-                  <Text fontSize='xs' fontStyle='italic' color='grey'>
+                  <Text fontSize="xs" fontStyle="italic" color="grey">
                     *A unique identifier or tracking number is assigned to the
                     dispute report for future reference. <br />
-                    *Homeowners can view their dispute report submission in the{' '}
+                    *Homeowners can view their dispute report submission in the{" "}
                     <Link
-                      href='/admin/disputes/submitted-disputes'
-                      color='blue.500'
-                      textDecor='underline'
+                      href="/admin/disputes/submitted-disputes"
+                      color="blue.500"
+                      textDecor="underline"
                     >
                       Submitted Dispute Reports
-                    </Link>{' '}
+                    </Link>{" "}
                     in the Dispute Resolution module.
                     <br />
                     *The upcoming review process and potential follow-up actions
@@ -163,12 +163,12 @@ export default function DisputeResolution () {
                   </Text>
                 )}
                 {activeStep === 1 && (
-                  <Text fontSize='xs' fontStyle='italic' color='grey'>
+                  <Text fontSize="xs" fontStyle="italic" color="grey">
                     *For the purpose of directing the resolution process more
                     effectively, the Officer reviews the HOA's rules and
-                    regulations indicated in the <DisputeBylaws /> before the
-                    scheduled meeting date. <br />
-                    *The standard meeting date for disputes is the{' '}
+                    regulations indicated in the <DisputeBylaws hoa={hoa} />{" "}
+                    before the scheduled meeting date. <br />
+                    *The standard meeting date for disputes is the{" "}
                     <b>upcoming Saturday</b> for the disputing parties to have
                     the opportunity to gather necessary evidence to support
                     their claims. <br />
@@ -177,23 +177,23 @@ export default function DisputeResolution () {
                   </Text>
                 )}
                 {activeStep === 2 && (
-                  <Text fontSize='xs' fontStyle='italic' color='grey'>
+                  <Text fontSize="xs" fontStyle="italic" color="grey">
                     *If there is a violation involved, the corresponding penalty
                     fee is added to the dispute parties' statement of account
                     and they are duly notified. the alleged violator/s can
-                    access the procedure on how to{' '}
+                    access the procedure on how to{" "}
                     <Link
-                      href='#payPenaltyFee'
-                      color='blue.500'
-                      textDecor='underline'
+                      href="#payPenaltyFee"
+                      color="blue.500"
+                      textDecor="underline"
                     >
                       pay the penalty fee
-                    </Link>{' '}
+                    </Link>{" "}
                     in the Violation Process Guide in the Violation Enforcement
                     module.
                     <br />
                     <br />
-                    <Text fontSize='xs' color='red.700'>
+                    <Text fontSize="xs" color="red.700">
                       *In such cases where a dispute cannot be resolved through
                       amicable means with the involvement of the board of
                       directors, the dispute can be elevated to the barangay.
@@ -205,7 +205,7 @@ export default function DisputeResolution () {
           </Card>
         </Box>
       </Flex>
-      <Box fontSize='sm' fontFamily='font.body' mt='1rem'>
+      <Box fontSize="sm" fontFamily="font.body" mt="1rem">
         <Text>
           The HOA maintains a comprehensive record of dispute reports, actions
           taken, and penalty fees imposed.
@@ -226,5 +226,5 @@ export default function DisputeResolution () {
         </UnorderedList>
       </Box>
     </div>
-  )
+  );
 }
--- app/(routes)/admin/disputes/process-guide/page.tsx
+++ app/(routes)/admin/disputes/process-guide/page.tsx
@@ -1,30 +1,37 @@
-'use client'
-import { Box, Button, Flex, Stack } from '@chakra-ui/react'
-import { Heading } from '@/components/ui/heading'
-import { Separator } from '@/components/ui/separator'
-import DisputeResolution from './_components/DisputeResolution'
-import Link from 'next/link'
+import { Box, Button, Flex, Stack } from "@chakra-ui/react";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import DisputeResolution from "./_components/DisputeResolution";
+import Link from "next/link";
+import { Hoa } from "@prisma/client";
+import { getHoaInfo } from "@/server/data/hoa-info";
 
-export default function DisputeProcess () {
+export default async function DisputeProcess() {
+  const hoa = await getHoaInfo();
+
+  if (!hoa) {
+    return null;
+  }
+
   return (
     <div>
-      <Flex justifyContent='space-between'>
+      <Flex justifyContent="space-between">
         <Heading
-          title='Dispute Resolution Process Guide'
-          description='Read more about the dispute resolution process. '
+          title="Dispute Resolution Process Guide"
+          description="Read more about the dispute resolution process. "
         />
-        <Button as={Link} href='/admin/disputes' size='sm'>
+        <Button as={Link} href="/admin/disputes" size="sm">
           Go Back
         </Button>
       </Flex>
-      <Separator className='mt-4 mb-6' />
+      <Separator className="mt-4 mb-6" />
 
-      <Stack spacing='50px'>
+      <Stack spacing="50px">
         {/* Dispute Resolution */}
-        <Box id='disputeResolution'>
-          <DisputeResolution />
+        <Box id="disputeResolution">
+          <DisputeResolution hoa={hoa} />
         </Box>
       </Stack>
     </div>
-  )
+  );
 }
