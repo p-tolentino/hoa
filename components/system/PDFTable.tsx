6d77d0ba9271ec9058961ca96795617b7bb94c61
kath <kathleen_tan@dlsu.edu.ph>
2024-03-20 00:04:41 +0800

    improved PDFTable format

--- components/system/PDFTable.tsx
+++ components/system/PDFTable.tsx
@@ -8,6 +8,7 @@ import {
   Heading,
   Box,
   Stack,
+  Text,
 } from "@chakra-ui/react";
 
 export default function PDFTable() {
@@ -15,22 +16,46 @@ export default function PDFTable() {
     <>
       <Box p="5%">
         <Stack spacing={3}>
-          <Heading fontSize="sm" fontFamily="font.heading">
-            Admin Directory Report
+          <Heading fontSize="md" fontFamily="font.heading">
+            Homeowners Association Name
           </Heading>
+          <Stack>
+            <Heading fontSize="sm" fontFamily="font.heading">
+              Homeowners Directory Report
+            </Heading>
+            <Text fontSize="sm">View the Homeowners Directory.</Text>
+          </Stack>
           <Table variant="striped">
             <Thead>
               <Th>Position</Th>
               <Th>Name</Th>
               <Th>Contact Number</Th>
-              <Th>Email</Th>
+              <Th>House No. & Street</Th>
+              <Th>Email Address</Th>
             </Thead>
             <Tbody>
               <Tr fontSize="xs">
-                <Td>Superuser</Td>
-                <Td>Superuser M. One</Td>
+                <Td>Member</Td>
+                <Td>Juan M. Dela Cruz</Td>
                 <Td>99596608231</Td>
-                <Td>admin@test.com</Td>
+                <Td>Sample Address</Td>
+                <Td>user1@envsec.com</Td>
+              </Tr>
+
+              <Tr fontSize="xs">
+                <Td>Member</Td>
+                <Td>Mark Garcia</Td>
+                <Td>09059099345</Td>
+                <Td>Sample Address</Td>
+                <Td>user2@envsec.com</Td>
+              </Tr>
+
+              <Tr fontSize="xs">
+                <Td>Member</Td>
+                <Td>James Gonzales</Td>
+                <Td>09175660423</Td>
+                <Td>Sample Address</Td>
+                <Td>user3@envsec.com</Td>
               </Tr>
             </Tbody>
           </Table>
