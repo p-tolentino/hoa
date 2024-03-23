bc8982910b4d897ef0feefdcd72705db0f53502d
kath <kathleen_tan@dlsu.edu.ph>
2024-03-23 17:00:17 +0800

    admin setup initialized (system admin settings)

--- app/(routes)/admin/settings/page.tsx
+++ app/(routes)/admin/settings/page.tsx
@@ -1,10 +1,21 @@
-import { Button } from "@/components/ui/button";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
+import { Stack, Button, Text, Input, HStack } from "@chakra-ui/react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import Link from "next/link";
 import { RxArrowRight as RightArrow } from "react-icons/rx";
+import { Hoa } from "@prisma/client";
+import PdfUpload from "./_components/pdf-upload";
 
-const Settings = async () => {
+const Settings = async ({ hoa }: { hoa: Hoa }) => {
   return (
     <div>
       <div className="flex justify-between">
@@ -13,13 +24,74 @@ const Settings = async () => {
           description="Configure your HOA's settings and preferences"
         />
         <Link href="/user/settings">
-          <Button className="text-black bg-yellow-400 end hover:bg-yellow-500">
+          <Button size="sm" colorScheme="yellow">
             User Settings
             <RightArrow className="w-4 h-4 ml-2" />
           </Button>
         </Link>
       </div>
       <Separator className="mt-4 mb-6" />
+      <Stack spacing={8}>
+        <HStack gap={5}>
+          <Text fontSize="sm" fontWeight="semibold">
+            Name of the Homeowners Association:
+          </Text>
+          <Input
+            w="60%"
+            size="sm"
+            type="string"
+            fontFamily="font.body"
+            placeholder="Homeowners Association Name"
+          />
+        </HStack>
+        <HStack gap={5}>
+          <Text fontSize="sm" fontWeight="semibold">
+            Contact Number of the Homeowners Association:
+          </Text>
+          <Input
+            w="40%"
+            size="sm"
+            type="string"
+            fontFamily="font.body"
+            placeholder="Homeowners Association Contact Number"
+          />
+        </HStack>
+        <HStack gap={5}>
+          <Text fontSize="sm" fontWeight="semibold">
+            Funds:
+          </Text>
+          <Input
+            w="30%"
+            size="sm"
+            type="number"
+            fontFamily="font.body"
+            placeholder="Php 1,000,000.00"
+          />
+        </HStack>
+        <HStack gap={5}>
+          <Text fontSize="sm" fontWeight="semibold">
+            Upload Homeowners' Association Bylaws:
+          </Text>
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button w="10%" size="xs" fontWeight="semibold">
+                Upload Bylaws
+              </Button>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Upload Homeowners' Association Bylaws</DialogTitle>
+                <DialogDescription>
+                  "Upload the latest Homeowners' Association Bylaws for members
+                  to view."
+                </DialogDescription>
+              </DialogHeader>
+              {/* File input */}
+              <PdfUpload hoa={hoa} />
+            </DialogContent>
+          </Dialog>
+        </HStack>
+      </Stack>
     </div>
   );
 };
