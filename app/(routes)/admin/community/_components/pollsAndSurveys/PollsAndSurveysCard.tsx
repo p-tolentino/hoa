279d0ca053967daf22f18fe0fad398a03c767760
kath <kathleen_tan@dlsu.edu.ph>
2024-03-25 15:48:28 +0800

    adjusted cardheight of polls&surveys tab

--- app/(routes)/admin/community/_components/pollsAndSurveys/PollsAndSurveysCard.tsx
+++ app/(routes)/admin/community/_components/pollsAndSurveys/PollsAndSurveysCard.tsx
@@ -1,35 +1,35 @@
-'use client'
+"use client";
 
 import {
   Card,
   CardContent,
   CardDescription,
   CardHeader,
-  CardTitle
-} from '@/components/ui/card'
-import { Input, Flex, Spacer, HStack } from '@chakra-ui/react'
+  CardTitle,
+} from "@/components/ui/card";
+import { Input, Flex, Spacer, HStack } from "@chakra-ui/react";
 import {
   Select,
   SelectContent,
   SelectGroup,
   SelectItem,
   SelectTrigger,
-  SelectValue
-} from '@/components/ui/select'
-import React from 'react'
-import { ScrollArea } from '@radix-ui/react-scroll-area'
+  SelectValue,
+} from "@/components/ui/select";
+import React from "react";
+import { ScrollArea } from "@radix-ui/react-scroll-area";
 
-import Post from './_components/post'
-import Create from './_components/create'
-import { useState } from 'react'
+import Post from "./_components/post";
+import Create from "./_components/create";
+import { useState } from "react";
 
-import { Polls, User } from '@prisma/client'
+import { Polls, User } from "@prisma/client";
 import { getPersonalInfo } from "@/server/data/user-info";
 import { useEffect } from "react";
 
 interface PollProps {
-  polls: Polls[]
-  user: string
+  polls: Polls[];
+  user: string;
 }
 
 interface UserInfo {
@@ -42,90 +42,92 @@ interface UserInfos {
   [userId: string]: UserInfo | null;
 }
 
-export default function PollsAndSurveysCard ({ polls, user }: PollProps) {
-  const [selectedCategory, setSelectedCategory] = useState('showAll')
-  const [selectedStatus, setSelectedStatus] = useState('All')
-  const [searchInput, setSearchInput] = useState('')
+export default function PollsAndSurveysCard({ polls, user }: PollProps) {
+  const [selectedCategory, setSelectedCategory] = useState("showAll");
+  const [selectedStatus, setSelectedStatus] = useState("All");
+  const [searchInput, setSearchInput] = useState("");
 
   const filteredPolls = polls
     .filter(
-      poll =>
-        selectedCategory === 'showAll' || poll.category === selectedCategory
+      (poll) =>
+        selectedCategory === "showAll" || poll.category === selectedCategory
     )
-    .filter(poll =>
+    .filter((poll) =>
       poll.title.toLowerCase().includes(searchInput.toLowerCase())
     )
-    .filter(poll => selectedStatus === 'All' || poll.status === selectedStatus)
+    .filter(
+      (poll) => selectedStatus === "All" || poll.status === selectedStatus
+    );
 
-    const [usersInfo, setUsersInfo] = useState<UserInfos>({});
+  const [usersInfo, setUsersInfo] = useState<UserInfos>({});
 
-    useEffect(() => {
-      const fetchUserInfos = async () => {
-        // Extract unique userIds from polls to avoid redundant fetches
-        const uniqueUserIds = Array.from(
-          new Set(polls.map((post) => post.userId))
-        );
-  
-        // Fetch user info for each unique userId
-        const userInfoPromises = uniqueUserIds.map(async (userId) => {
-          const userInfo = await getPersonalInfo(userId);
-          return { userId, userInfo };
-        });
-  
-        // Resolve all promises and update state
-        const userInfosArray = await Promise.all(userInfoPromises);
-        const userInfosObj = userInfosArray.reduce<UserInfos>(
-          (acc, { userId, userInfo }) => {
-            acc[userId] = userInfo;
-            return acc;
-          },
-          {}
-        );
-  
-        setUsersInfo(userInfosObj);
-      };
-  
-      fetchUserInfos();
-    }, [polls]);
+  useEffect(() => {
+    const fetchUserInfos = async () => {
+      // Extract unique userIds from polls to avoid redundant fetches
+      const uniqueUserIds = Array.from(
+        new Set(polls.map((post) => post.userId))
+      );
+
+      // Fetch user info for each unique userId
+      const userInfoPromises = uniqueUserIds.map(async (userId) => {
+        const userInfo = await getPersonalInfo(userId);
+        return { userId, userInfo };
+      });
+
+      // Resolve all promises and update state
+      const userInfosArray = await Promise.all(userInfoPromises);
+      const userInfosObj = userInfosArray.reduce<UserInfos>(
+        (acc, { userId, userInfo }) => {
+          acc[userId] = userInfo;
+          return acc;
+        },
+        {}
+      );
+
+      setUsersInfo(userInfosObj);
+    };
+
+    fetchUserInfos();
+  }, [polls]);
 
   return (
     <>
-      <Card className='h-[75vh]'>
-        <Flex justifyContent='space-between'>
+      <Card className="h-[70vh]">
+        <Flex justifyContent="space-between">
           <CardHeader>
             <CardTitle>Polls & Surveys</CardTitle>
             <CardDescription>
               View and answer polls and surveys posted by HOA Officers.
             </CardDescription>
           </CardHeader>
-          <HStack p='20px'>
+          <HStack p="20px">
             {/* Create Poll/Survey Button */}
             <Create />
           </HStack>
         </Flex>
-        <CardContent className='space-y-2'>
-          <Flex justifyContent='space-between' mb='1%'>
+        <CardContent className="space-y-2">
+          <Flex justifyContent="space-between" mb="1%">
             <Input
-              size='sm'
-              w='35%'
-              type='string'
-              placeholder='Search by Poll or Survey Title'
+              size="sm"
+              w="35%"
+              type="string"
+              placeholder="Search by Poll or Survey Title"
               value={searchInput}
-              onChange={e => setSearchInput(e.target.value)}
+              onChange={(e) => setSearchInput(e.target.value)}
             />
             <Spacer />
             <HStack>
               <Select value={selectedStatus} onValueChange={setSelectedStatus}>
-                <SelectTrigger className='w-[150px]'>
-                  <SelectValue placeholder='Status' />
+                <SelectTrigger className="w-[150px]">
+                  <SelectValue placeholder="Status" />
                 </SelectTrigger>
-                <SelectContent className='sm:w-40'>
+                <SelectContent className="sm:w-40">
                   <SelectGroup>
-                    <SelectItem value='All' className='font-semibold'>
+                    <SelectItem value="All" className="font-semibold">
                       Status
                     </SelectItem>
-                    <SelectItem value='ACTIVE'>Active</SelectItem>
-                    <SelectItem value='INACTIVE'>Inactive</SelectItem>
+                    <SelectItem value="ACTIVE">Active</SelectItem>
+                    <SelectItem value="INACTIVE">Inactive</SelectItem>
                   </SelectGroup>
                 </SelectContent>
               </Select>
@@ -135,19 +137,19 @@ export default function PollsAndSurveysCard ({ polls, user }: PollProps) {
                 value={selectedCategory}
                 onValueChange={setSelectedCategory}
               >
-                <SelectTrigger className='w-[150px]'>
-                  <SelectValue placeholder='Show All' />
+                <SelectTrigger className="w-[150px]">
+                  <SelectValue placeholder="Show All" />
                 </SelectTrigger>
                 <SelectContent>
                   <SelectGroup>
-                    <SelectItem value='showAll' className='font-semibold'>
+                    <SelectItem value="showAll" className="font-semibold">
                       Show All
                     </SelectItem>
-                    <SelectItem value='MEETING'>Meeting</SelectItem>
-                    <SelectItem value='ELECTION'>Election</SelectItem>
-                    <SelectItem value='INQUIRY'>Inquiry</SelectItem>
-                    <SelectItem value='EVENT'>Event</SelectItem>
-                    <SelectItem value='OTHER'>Other</SelectItem>
+                    <SelectItem value="MEETING">Meeting</SelectItem>
+                    <SelectItem value="ELECTION">Election</SelectItem>
+                    <SelectItem value="INQUIRY">Inquiry</SelectItem>
+                    <SelectItem value="EVENT">Event</SelectItem>
+                    <SelectItem value="OTHER">Other</SelectItem>
                   </SelectGroup>
                 </SelectContent>
               </Select>
@@ -156,12 +158,12 @@ export default function PollsAndSurveysCard ({ polls, user }: PollProps) {
 
           {/* Wrap the PollPosts and SurveyPosts inside ScrollArea */}
           <ScrollArea
-            style={{ maxHeight: 'calc(70vh - 180px)', overflowY: 'auto' }}
+            style={{ maxHeight: "calc(70vh - 180px)", overflowY: "auto" }}
           >
-            <Post polls={filteredPolls} user={user}  userInfos={usersInfo}/>
+            <Post polls={filteredPolls} user={user} userInfos={usersInfo} />
           </ScrollArea>
         </CardContent>
       </Card>
     </>
-  )
+  );
 }
